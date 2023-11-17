bc3eb2b921d29de2dac614ec277a9df01e0f9f68
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-11-17 13:13:51 +0100

    Wezterm config cleanup

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,41 +1,31 @@
 local wezterm = require('wezterm')
+local act = wezterm.action
 local colors = require('colors')
 local color_schemes = require('color_schemes')
 
 wezterm.on('update-right-status', function(window, pane)
-  -- Each element holds the text for a cell in a "powerline" style << fade
   local cells = {}
 
-  -- Figure out the cwd and host of the current pane.
-  -- This will pick up the hostname for the remote host if your
-  -- shell is using OSC 7 on the remote host.
   local cwd_uri = pane:get_current_working_dir()
   if cwd_uri then
     local cwd = ''
     local hostname = ''
 
     if type(cwd_uri) == 'userdata' then
-      -- Running on a newer version of wezterm and we have
-      -- a URL object here, making this simple!
-
       cwd = cwd_uri.file_path
       hostname = cwd_uri.host or wezterm.hostname()
     else
-      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
-      -- which doesn't have the Url object
       cwd_uri = cwd_uri:sub(8)
       local slash = cwd_uri:find '/'
       if slash then
         hostname = cwd_uri:sub(1, slash - 1)
-        -- and extract the cwd from the uri, decoding %-encoding
         cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
           return string.char(tonumber(hex, 16))
         end)
       end
     end
 
-    -- Remove the domain name portion of the hostname
-    local dot = hostname:find '[.]'
+    local dot = hostname:find('[.]')
     if dot then
       hostname = hostname:sub(1, dot - 1)
     end
@@ -47,16 +37,14 @@ wezterm.on('update-right-status', function(window, pane)
     table.insert(cells, hostname)
   end
 
-  -- I like my date/time in this style: "Wed Mar 3 08:14"
-  local date = wezterm.strftime('%a %b %-d %H:%M')
+  local date = wezterm.strftime('%a %-d %b - %H:%M:%S')
   table.insert(cells, date)
 
-  -- An entry for each battery (typically 0 or 1 battery)
   for _, b in ipairs(wezterm.battery_info()) do
     table.insert(cells, string.format('%.0f%%', b.state_of_charge * 100))
   end
 
-  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+  -- local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
   local statusline_colors = {
     colors.dark_palette.bg0,
@@ -70,7 +58,6 @@ wezterm.on('update-right-status', function(window, pane)
   local elements = {}
   local num_cells = 0
 
-  -- Translate a cell into elements
   local function push(text, is_last)
     local cell_no = num_cells + 1
     table.insert(elements, {Foreground = {Color = text_fg}})
@@ -78,7 +65,7 @@ wezterm.on('update-right-status', function(window, pane)
     table.insert(elements, {Text = ' ' .. text .. ' '})
     if not is_last then
       table.insert(elements, {Foreground = {Color = statusline_colors[cell_no + 1]}})
-      table.insert(elements, {Text = SOLID_LEFT_ARROW})
+      -- table.insert(elements, {Text = SOLID_LEFT_ARROW})
     end
     num_cells = num_cells + 1
   end
@@ -165,7 +152,12 @@ return {
     {
       key = 'e',
       mods = 'CTRL',
-      action = wezterm.action.EmitEvent('toggle-colorscheme'),
+      action = act.EmitEvent('toggle-colorscheme'),
+    },
+    {
+      key = 'k',
+      mods = 'CMD',
+      action = act.ClearScrollback('ScrollbackAndViewport'),
     },
   },
 }
