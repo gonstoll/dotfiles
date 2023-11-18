dd0ed9774dfbff920e4cb61acec86d239b4234bc
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-11-18 18:24:35 +0100

    Opening oil as float

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,9 +1,19 @@
 return {
   'stevearc/oil.nvim',
   dependencies = {{'nvim-tree/nvim-web-devicons', lazy = true}},
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, bufnr)
+        return vim.startswith(name, '.DS_Store')
+      end,
+    },
+    float = {
+      padding = 4,
+    },
+  },
   config = function(_, opts)
     require('oil').setup(opts)
-    vim.keymap.set('n', '-', require('oil').open, {desc = 'Open parent directory'})
+    vim.keymap.set('n', '-', require('oil').open_float, {desc = 'Open parent directory'})
   end
 }
