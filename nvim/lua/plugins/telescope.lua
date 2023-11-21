e00083a460359fcf8ae1edf3ca132c8375c08bf6
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-11-21 16:16:15 +0100

    Lazy loading telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,123 @@
+local keys = {
+  {
+    desc = 'Search Files',
+    lhs = 'sf',
+    rhs = '<cmd>lua require("telescope").extensions.file_browser.file_browser({path = "%:p:h", hidden = true})<CR>',
+  },
+  {
+    desc = 'Find files respecting gitignore',
+    lhs = ';f',
+    rhs = '<cmd>lua require("telescope.builtin").find_files({no_ignore = true, hidden = true})<CR>',
+  },
+  {
+    desc = 'Get file diagnostics',
+    lhs = ';se',
+    rhs = '<cmd>lua require("telescope.builtin").diagnostics({bufnr = 0})<CR>',
+  },
+  {
+    desc = 'Search in Current buffer Directory',
+    lhs = ';cd',
+    rhs = '<cmd>lua require("telescope.builtin").find_files({cwd = require("telescope.utils").buffer_dir()})<CR>',
+  },
+  {
+    desc = 'Search Files',
+    lhs = ';sf',
+    rhs = '<cmd>lua require("telescope.builtin").find_files()<CR>',
+  },
+  {
+    desc = 'Search by Grep',
+    lhs = ';sg',
+    rhs = '<cmd>lua require("telescope.builtin").live_grep()<CR>',
+  },
+  {
+    desc = 'Search Git Files',
+    lhs = ';gf',
+    rhs = '<cmd>lua require("telescope.builtin").git_files()<CR>',
+  },
+  {
+    desc = 'Keymaps',
+    lhs = ';sk',
+    rhs = '<cmd>lua require("telescope.builtin").keymaps()<CR>',
+  },
+  {
+    desc = 'Search Diagnostics',
+    lhs = ';sd',
+    rhs = '<cmd>lua require("telescope.builtin").diagnostics()<CR>',
+  },
+  {
+    desc = 'Search current Word',
+    lhs = ';sw',
+    rhs = '<cmd>lua require("telescope.builtin").grep_string()<CR>',
+  },
+  {
+    desc = 'Find recently opened files',
+    lhs = ';?',
+    rhs = '<cmd>lua require("telescope.builtin").oldfiles()<CR>',
+  },
+  {
+    desc = 'Find opened buffers in current neovim instance',
+    lhs = ';y',
+    rhs = '<cmd>lua require("telescope.builtin").buffers()<CR>',
+  },
+  {
+    desc = 'Search Help',
+    lhs = ';sh',
+    rhs = '<cmd>lua require("telescope.builtin").help_tags()<CR>',
+  },
+  {
+    desc = 'Search Colorscheme',
+    lhs = ';sc',
+    rhs = '<cmd>lua require("telescope.builtin").colorscheme()<CR>',
+  },
+  {
+    desc = 'Get list of searches',
+    lhs = ';ss',
+    rhs = '<cmd>lua require("telescope.builtin").search_history()<CR>',
+  },
+  {
+    desc = 'Fuzzily search in current buffer',
+    lhs = ';/',
+    rhs = '<cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>',
+  },
+  {
+    desc = 'Resume',
+    lhs = ';;',
+    rhs = '<cmd>lua require("telescope").extensions.resume.resume()<CR>',
+  },
+  {
+    desc = 'Document Symbols',
+    lhs = '<leader>ds',
+    rhs = '<cmd>lua require("telescope.builtin").lsp_document_symbols()<CR>',
+  },
+  {
+    desc = 'Workspace Symbols',
+    lhs = '<leader>ws',
+    rhs = '<cmd>lua require("telescope.builtin").lsp_dynamic_workspace_symbols()<CR>',
+  },
+  {
+    desc = 'Goto References',
+    lhs = 'gr',
+    rhs = '<cmd>lua require("telescope.builtin").lsp_references()<CR>',
+  },
+}
+
+local function get_desc(description)
+  return description .. ' (Telescope)'
+end
+
+local function get_lazy_keys()
+  local lazy_keys = {}
+  for _, key in ipairs(keys) do
+    local desc = get_desc(key.desc)
+    table.insert(lazy_keys, {key.lhs, key.rhs, desc = desc})
+  end
+  return lazy_keys
+end
+
 return {
   {
     'nvim-telescope/telescope-file-browser.nvim',
-    event = 'BufReadPre',
+    lazy = true,
     dependencies = {'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim'},
   },
 
@@ -12,11 +128,9 @@ return {
       'nvim-lua/plenary.nvim',
       {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
     },
-    event = 'BufReadPre',
+    keys = get_lazy_keys(),
     config = function()
       local telescope = require('telescope')
-      local builtin = require('telescope.builtin')
-      local utils = require('telescope.utils')
       local actions = require('telescope.actions')
 
       local fb_actions = telescope.extensions.file_browser.actions
@@ -53,46 +167,9 @@ return {
       telescope.load_extension('fzf')
       telescope.load_extension('file_browser')
 
-      vim.keymap.set('n', 'sf', function()
-        telescope.extensions.file_browser.file_browser({
-          path = '%:p:h',
-          hidden = true,
-        })
-      end, {desc = '[S]earch [F]iles (Telescope file browser)'})
-
-      local function telescope_keymap(map, command, desc)
-        local description = desc .. ' (Telescope)'
-        vim.keymap.set('n', map, command, {desc = description})
+      for _, key in ipairs(keys) do
+        vim.keymap.set('n', key.lhs, key.rhs, {desc = get_desc(key.desc)})
       end
-
-      telescope_keymap(';f', function()
-        builtin.find_files({
-          no_ignore = true,
-          hidden = true
-        })
-      end, 'Find files respecting gitignore')
-
-      telescope_keymap(';se', function()
-        builtin.diagnostics({bufnr = 0})
-      end, 'Get file diagnostics')
-
-      telescope_keymap(';cd', function()
-        builtin.find_files({cwd = utils.buffer_dir()})
-      end, 'Search in Current buffer Directory')
-
-      telescope_keymap(';sf', builtin.find_files, 'Search Files')
-      telescope_keymap(';sg', builtin.live_grep, 'Search by Grep')
-      telescope_keymap(';gf', builtin.git_files, 'Search Git Files')
-      telescope_keymap(';sk', ':Telescope keymaps<CR>', 'Keymaps')
-      telescope_keymap(';sd', builtin.diagnostics, 'Search Diagnostics')
-      telescope_keymap(';sw', builtin.grep_string, 'Search current Word')
-      telescope_keymap(';?', builtin.oldfiles, 'Find recently opened files')
-      telescope_keymap(';y', builtin.buffers, 'Find opened buffers in current neovim instance')
-      telescope_keymap(';sh', builtin.help_tags, 'Search Help')
-      telescope_keymap(';sc', builtin.colorscheme, 'Search Colorscheme')
-      telescope_keymap(';ss', builtin.search_history, 'Get list of searches')
-      telescope_keymap(';/', builtin.current_buffer_fuzzy_find, 'Fuzzily search in current buffer')
-      telescope_keymap(';;', builtin.resume, 'Resume')
     end,
   }
 }
