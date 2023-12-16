f8bc100def42fb4deb7d63b85cec1daea5ac4cdb
Gonzalo Stoll <gonzalostoll@gonzalo-2.local>
2023-12-16 17:28:00 +0100

    Adding config for initial tmux plugin installations

--- bin/runtime.sh
+++ bin/runtime.sh
@@ -4,3 +4,6 @@ echo "> Installing runtime dependencies"
 
 # NPM global packages
 npm install -g depcheck eslint prettier prettier-plugin-tailwindss typescript typescript-language-server vscode-langservers-extracted
+
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+~/.config/tmux/plugins/tpm/bin/install_plugins
