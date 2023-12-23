fe0d92861b628c09399e60709ad1db9b4ec97825
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-23 10:01:02 +0100

    Trying out harpoon

---
+++ nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,57 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = {{'nvim-lua/plenary.nvim'}},
+  config = function()
+    local harpoon = require('harpoon')
+    local utils = require('utils')
+
+    local function km(rhs, command, opts)
+      vim.keymap.set(
+        'n',
+        rhs,
+        command,
+        utils.mergeTable(opts, {desc = opts.desc and 'Harpoon: ' .. opts.desc or nil})
+      )
+    end
+
+    harpoon:setup()
+
+    harpoon:extend({
+      UI_CREATE = function(cx)
+        km(
+          '<C-v>',
+          function() harpoon.ui:select_menu_item({vsplit = true}) end,
+          {desc = 'Open as vertical split', buffer = cx.bufnr}
+        )
+        km(
+          '<C-x>',
+          function() harpoon.ui:select_menu_item({split = true}) end,
+          {desc = 'Open as split', buffer = cx.bufnr}
+        )
+        km(
+          '<C-t>',
+          function() harpoon.ui:select_menu_item({tabedit = true}) end,
+          {desc = 'Open in new tab', buffer = cx.bufnr}
+        )
+      end,
+    })
+
+    km(';a', function() harpoon:list():append() end, {desc = 'Add to list'})
+    km(';h', function() harpoon.ui:toggle_quick_menu(harpoon:list(), {border = 'rounded'}) end, {desc = 'Open menu'})
+
+    km(';1', function() harpoon:list():select(1) end, {desc = 'Select file 1'})
+    km(';2', function() harpoon:list():select(2) end, {desc = 'Select file 2'})
+    km(';3', function() harpoon:list():select(3) end, {desc = 'Select file 3'})
+    km(';4', function() harpoon:list():select(4) end, {desc = 'Select file 4'})
+    km(';5', function() harpoon:list():select(5) end, {desc = 'Select file 5'})
+    km(';6', function() harpoon:list():select(6) end, {desc = 'Select file 6'})
+    km(';7', function() harpoon:list():select(7) end, {desc = 'Select file 7'})
+    km(';8', function() harpoon:list():select(8) end, {desc = 'Select file 8'})
+    km(';9', function() harpoon:list():select(9) end, {desc = 'Select file 9'})
+    -- If I ever have more than 9 tabs open, time to rethink my life choices
+
+    km('<S-h>', function() harpoon:list():prev() end, {desc = 'Previous file'})
+    km('<S-l>', function() harpoon:list():next() end, {desc = 'Next file'})
+  end
+}
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,59 +4,15 @@ return {
     {'nvim-treesitter/nvim-treesitter'},
     {'nvim-tree/nvim-web-devicons', lazy = true},
   },
-  keys = {
-    {
-      '-',
-      '<cmd>lua require("oil").open_float()<CR>',
-      desc = 'Open parent directory',
-    },
-  },
-  init = function()
-    local netrw_bufname
-
-    pcall(vim.api.nvim_clear_autocmds, {group = 'FileExplorer'})
-
-    vim.api.nvim_create_autocmd('VimEnter', {
-      pattern = '*',
-      once = true,
-      callback = function()
-        pcall(vim.api.nvim_clear_autocmds, {group = 'FileExplorer'})
-      end,
-    })
-
-    vim.api.nvim_create_autocmd('BufEnter', {
-      group = vim.api.nvim_create_augroup('oil.nvim', {clear = true}),
-      pattern = '*',
-      callback = function()
-        vim.schedule(function()
-          if vim.bo[0].filetype == 'netrw' then
-            return
-          end
-          local bufname = vim.api.nvim_buf_get_name(0)
-          if vim.fn.isdirectory(bufname) == 0 then
-            _, netrw_bufname = pcall(vim.fn.expand, '#:p:h')
-            return
-          end
-
-          -- prevents reopening of file-browser if exiting without selecting a file
-          if netrw_bufname == bufname then
-            netrw_bufname = nil
-            return
-          else
-            netrw_bufname = bufname
-          end
-
-          -- ensure no buffers remain with the directory name
-          vim.api.nvim_buf_set_option(0, 'bufhidden', 'wipe')
-
-          require('oil').open_float()
-        end)
-      end,
-      desc = 'oil.nvim replacement for netrw',
-    })
-  end,
+  -- keys = {
+  --   {
+  --     '-',
+  --     '<cmd>lua require("oil").open_float()<CR>',
+  --     desc = 'Open parent directory',
+  --   },
+  -- },
   opts = {
-    default_file_explorer = false,
+    default_file_explorer = true,
     view_options = {
       show_hidden = true,
       is_always_hidden = function(name, bufnr)
@@ -69,6 +25,6 @@ return {
   },
   config = function(_, opts)
     require('oil').setup(opts)
-    vim.keymap.set('n', '-', require('oil').open_float, {desc = 'Open parent directory'})
+    vim.keymap.set('n', '-', require('oil').open, {desc = 'Open parent directory'})
   end
 }
