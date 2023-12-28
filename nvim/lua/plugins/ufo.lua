5ce5e710f7a39c2c60280ca5b58cfa28b78c4214
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-28 10:59:22 +0100

    Custom fold text

--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,3 +1,34 @@
+-- Custom fold text handler for ufo showing line count folded
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, {chunkText, hlGroup})
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then
+        suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+      end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+
+  table.insert(newVirtText, {suffix, 'MoreMsg'})
+  return newVirtText
+end
+
 return {
   {
     'luukvbaal/statuscol.nvim',
@@ -17,9 +48,10 @@ return {
     'kevinhwang91/nvim-ufo',
     dependencies = {'kevinhwang91/promise-async'},
     opts = {
+      fold_virt_text_handler = handler,
       provider_selector = function(bufnr, filetype, buftype)
         return {'treesitter', 'indent'}
-      end
+      end,
     },
     config = function(_, opts)
       local ufo = require('ufo')
