5ec0f7f4c360380ed234d74d6aa4801f0e1b4f0d
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-28 10:15:58 +0100

    Lualine updates

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,12 +1,9 @@
 return {
   'nvim-lualine/lualine.nvim',
   event = 'BufReadPre',
-  init = function()
-    vim.keymap.set('n', '<leader>l', ':lua require("lualine").refresh<CR>', {desc = 'Refresh status line'})
-  end,
   opts = function()
+    local icons = require('utils.icons')
     local lualineTheme = {}
-
     local isRosePineTheme = vim.g.colors_name == 'rose-pine'
 
     if isRosePineTheme then
@@ -45,127 +42,147 @@ return {
         },
       }
     else
-      local gruvboxConfiguration = vim.fn['gruvbox_material#get_configuration']()
+      local themeConfig = vim.fn['gruvbox_material#get_configuration']()
 
-      local gruvboxPalette = vim.fn['gruvbox_material#get_palette'](
-        gruvboxConfiguration.background,
-        gruvboxConfiguration.foreground,
-        gruvboxConfiguration.colors_override
+      local palette = vim.fn['gruvbox_material#get_palette'](
+        themeConfig.background,
+        themeConfig.foreground,
+        themeConfig.colors_override
       )
 
       lualineTheme = {
         normal = {
-          a = {bg = 'none', fg = gruvboxPalette.purple[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.purple[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         insert = {
-          a = {bg = 'none', fg = gruvboxPalette.green[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.green[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         visual = {
-          a = {bg = 'none', fg = gruvboxPalette.red[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.red[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         replace = {
-          a = {bg = 'none', fg = gruvboxPalette.yellow[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.yellow[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         command = {
-          a = {bg = 'none', fg = gruvboxPalette.blue[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.blue[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         terminal = {
-          a = {bg = 'none', fg = gruvboxPalette.aqua[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.fg0[1]},
+          a = {bg = 'none', fg = palette.aqua[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.fg0[1]},
         },
         inactive = {
-          a = {bg = 'none', fg = gruvboxPalette.grey2[1]},
-          b = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.grey2[1]},
-          c = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.grey2[1]},
-          z = {bg = gruvboxPalette.bg_statusline3[1], fg = gruvboxPalette.grey2[1]},
+          a = {bg = 'none', fg = palette.grey2[1]},
+          b = {bg = palette.bg_statusline3[1], fg = palette.grey2[1]},
+          c = {bg = palette.bg_statusline3[1], fg = palette.grey2[1]},
+          z = {bg = palette.bg_statusline3[1], fg = palette.grey2[1]},
         }
       }
     end
 
+    local mode = {
+      'mode',
+      color = {gui = ''},
+      padding = 0,
+      fmt = function(str)
+        local mode = str:sub(1, 1):upper() .. str:sub(2):lower()
+        return '[' .. mode .. ']'
+      end,
+    }
+
+    local diagnostics = {
+      'diagnostics',
+      sources = {'nvim_diagnostic'},
+      sections = {'error', 'warn', 'info', 'hint'},
+      colored = true,
+      update_in_insert = false,
+      always_visible = false,
+      symbols = {
+        error = icons.diagnostics.Error,
+        hint = icons.diagnostics.Hint,
+        info = icons.diagnostics.Info,
+        warn = icons.diagnostics.Warn,
+      },
+    }
+
+    local filename = {
+      'filename',
+      file_status = true,
+      newfile_status = true,
+      path = 4,
+      shorting_target = 40,
+      symbols = {
+        modified = icons.git.changed,
+        readonly = icons.git.deleted,
+        newfile = icons.git.added,
+      }
+    }
+
+    local diff = {
+      'diff',
+      colored = true,
+      always_visible = false,
+      symbols = {
+        added = icons.git.added .. ' ',
+        untracked = icons.git.added .. ' ',
+        modified = icons.git.changed .. ' ',
+        removed = icons.git.deleted .. ' ',
+      },
+      source = function()
+        local gitsigns = vim.b.gitsigns_status_dict
+        if gitsigns then
+          return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed,
+          }
+        end
+      end,
+    }
+
+    local filetype = {'filetype', colored = true}
+
     return {
       options = {
         theme = lualineTheme,
-        component_separators = '|',
+        component_separators = '',
         section_separators = '',
         globalstatus = true,
       },
       sections = {
-        lualine_a = {
-          {
-            'mode',
-            color = {gui = ''},
-            padding = 0,
-            fmt = function(str)
-              local mode = str:sub(1, 1):upper() .. str:sub(2):lower()
-              return '[' .. mode .. ']'
-            end,
-          }
-        },
-        lualine_b = {
-          {
-            'filename',
-            file_status = true,
-            path = 4,
-            shorting_target = 40,
-          },
-          'branch',
-          'diff',
-        },
-        lualine_c = {
-          {'diagnostics'},
-        },
+        lualine_a = {mode},
+        lualine_b = {filename, 'branch', diff},
+        lualine_c = {},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = {
-          {
-            'filetype',
-            colored = true,
-          },
-          'progress',
-          {
-            'location',
-            color = {gui = ''},
-          }
-        },
+        lualine_z = {diagnostics, filetype, 'progress'},
       },
       inactive_sections = {
-        lualine_a = {
-          {
-            'filename',
-            file_status = true,
-            path = 1,
-          },
-          'diagnostics',
-          'diff',
-        },
+        lualine_a = {filename, diagnostics, diff},
         lualine_b = {},
         lualine_c = {},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = {'location'},
+        lualine_z = {},
       },
       tabline = {},
-      extensions = {
-        'trouble',
-        'fzf',
-      },
+      extensions = {'trouble', 'fzf'},
     }
   end,
 }
