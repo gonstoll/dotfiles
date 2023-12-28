cb6848d00bfdb1277c46a69446705de7c12f2b1c
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-28 10:56:50 +0100

    Gitsigns icons and fold column tweaks

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -10,59 +10,69 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     event = 'BufReadPre',
-    opts = {
-      current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 500,
-      },
-      preview_config = {
-        border = 'rounded',
-        title = 'Preview changes',
-        title_pos = 'center',
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+    opts = function()
+      local icons = require('utils.icons')
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+      return {
+        signs = {
+          add = {text = icons.git.added},
+          change = {text = icons.git.changed},
+          delete = {text = icons.git.deleted},
+          topdelete = {text = icons.git.deleted},
+          changedelete = {text = icons.git.changed},
+          untracked = {text = icons.git.added},
+        },
+        current_line_blame = true,
+        current_line_blame_opts = {delay = 500},
+        preview_config = {
+          border = 'rounded',
+          title = 'Preview changes',
+          title_pos = 'center'
+        },
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
 
-        -- Navigation
-        map('n', ']c', function()
-          if vim.wo.diff then return ']c' end
-          vim.schedule(function() gs.next_hunk() end)
-          return '<Ignore>'
-        end, {expr = true, desc = 'Next hunk (Gitsigns)'})
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
 
-        map('n', '[c', function()
-          if vim.wo.diff then return '[c' end
-          vim.schedule(function() gs.prev_hunk() end)
-          return '<Ignore>'
-        end, {expr = true, desc = 'Previous hunk (Gitsigns)'})
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, {expr = true, desc = 'Next hunk (Gitsigns)'})
 
-        -- Actions
-        map('n', '<leader>hs', gs.stage_hunk, {desc = 'Stage hunk (Gitsigns)'})
-        map('n', '<leader>hr', gs.reset_hunk, {desc = 'Reset hunk (Gitsigns)'})
-        map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
-          {desc = 'Stage hunk (Gitsigns)'})
-        map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
-          {desc = 'Reset hunk (Gitsigns)'})
-        map('n', '<leader>hS', gs.stage_buffer, {desc = 'Stage buffer (Gitsigns)'})
-        map('n', '<leader>hu', gs.undo_stage_hunk, {desc = 'Undo stage hunk (Gitsigns)'})
-        map('n', '<leader>hR', gs.reset_buffer, {desc = 'Reset buffer (Gitsigns)'})
-        map('n', '<leader>hp', gs.preview_hunk, {desc = 'Preview hunk (Gitsigns)'})
-        map('n', '<leader>hb', function() gs.blame_line {full = true} end, {desc = 'Blame line (Gitsigns)'})
-        map('n', '<leader>tb', gs.toggle_current_line_blame, {desc = 'Toggle current line blame (Gitsigns)'})
-        map('n', '<leader>hd', gs.diffthis, {desc = 'Diff this (Gitsigns)'})
-        map('n', '<leader>hD', function() gs.diffthis('~') end, {desc = 'Diff this (Gitsigns)'})
-        map('n', '<leader>td', gs.toggle_deleted, {desc = 'Toggle deleted (Gitsigns)'})
+          map('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, {expr = true, desc = 'Previous hunk (Gitsigns)'})
 
-        -- Text object
-        map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-      end,
-    },
+          -- Actions
+          map('n', '<leader>hs', gs.stage_hunk, {desc = 'Stage hunk (Gitsigns)'})
+          map('n', '<leader>hr', gs.reset_hunk, {desc = 'Reset hunk (Gitsigns)'})
+          map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
+            {desc = 'Stage hunk (Gitsigns)'})
+          map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end,
+            {desc = 'Reset hunk (Gitsigns)'})
+          map('n', '<leader>hS', gs.stage_buffer, {desc = 'Stage buffer (Gitsigns)'})
+          map('n', '<leader>hu', gs.undo_stage_hunk, {desc = 'Undo stage hunk (Gitsigns)'})
+          map('n', '<leader>hR', gs.reset_buffer, {desc = 'Reset buffer (Gitsigns)'})
+          map('n', '<leader>hp', gs.preview_hunk, {desc = 'Preview hunk (Gitsigns)'})
+          map('n', '<leader>hb', function() gs.blame_line {full = true} end, {desc = 'Blame line (Gitsigns)'})
+          map('n', '<leader>tb', gs.toggle_current_line_blame, {desc = 'Toggle current line blame (Gitsigns)'})
+          map('n', '<leader>hd', gs.diffthis, {desc = 'Diff this (Gitsigns)'})
+          map('n', '<leader>hD', function() gs.diffthis('~') end, {desc = 'Diff this (Gitsigns)'})
+          map('n', '<leader>td', gs.toggle_deleted, {desc = 'Toggle deleted (Gitsigns)'})
+
+          -- Text object
+          map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+        end,
+      }
+    end,
     config = function(_, opts)
       local gitsigns = require('gitsigns')
 
--- nvim/lua/plugins/ufo.lua
+++ nvim/lua/plugins/ufo.lua
@@ -1,23 +1,39 @@
 return {
-  'kevinhwang91/nvim-ufo',
-  dependencies = {'kevinhwang91/promise-async'},
-  opts = {
-    provider_selector = function(bufnr, filetype, buftype)
-      return {'treesitter', 'indent'}
+  {
+    'luukvbaal/statuscol.nvim',
+    config = function()
+      local builtin = require('statuscol.builtin')
+      require('statuscol').setup({
+        segments = {
+          {text = {builtin.foldfunc}, click = 'v:lua.ScFa'},
+          {text = {' %s'}, click = 'v:lua.ScSa'},
+          {text = {builtin.lnumfunc, ' '}, condition = {true, builtin.not_empty}, click = 'v:lua.ScLa'},
+        },
+      })
+    end,
+  },
+
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {'kevinhwang91/promise-async'},
+    opts = {
+      provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+      end
+    },
+    config = function(_, opts)
+      local ufo = require('ufo')
+
+      ufo.setup(opts)
+
+      vim.keymap.set('n', 'zR', ufo.openAllFolds)
+      vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+      vim.keymap.set('n', 'K', function()
+        local winid = ufo.peekFoldedLinesUnderCursor(true)
+        if not winid then
+          vim.lsp.buf.hover()
+        end
+      end, {desc = 'LSP: Show hover documentation and folded code'})
     end
   },
-  config = function(_, opts)
-    local ufo = require('ufo')
-
-    ufo.setup(opts)
-
-    vim.keymap.set('n', 'zR', ufo.openAllFolds)
-    vim.keymap.set('n', 'zM', ufo.closeAllFolds)
-    vim.keymap.set('n', 'K', function()
-      local winid = ufo.peekFoldedLinesUnderCursor(true)
-      if not winid then
-        vim.lsp.buf.hover()
-      end
-    end, {desc = 'LSP: Show hover documentation and folded code'})
-  end
 }
