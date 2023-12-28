1dd0e1e273ad80b2959a8f82108fa047ba3ec088
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-28 12:01:36 +0100

    Adds cssmodules_ls

--- nvim/lua/plugins/lsp/lspconfig.lua
+++ nvim/lua/plugins/lsp/lspconfig.lua
@@ -140,6 +140,7 @@ M.setup = function()
           cssls = {capabilities = capabilities, on_attach = on_attach},
           emmet_language_server = {capabilities = capabilities, on_attach = on_attach},
           bashls = {capabilities = capabilities, on_attach = on_attach},
+          cssmodules_ls = {capabilities = capabilities, on_attach = on_attach},
           eslint = require('plugins.lsp.configs.eslint').setup(capabilities, on_attach),
           lua_ls = require('plugins.lsp.configs.lua_ls').setup(capabilities, on_attach),
           tsserver = require('plugins.lsp.configs.tsserver').setup(capabilities, on_attach),
