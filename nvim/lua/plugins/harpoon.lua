4f0732d229bdb40e6014e7e493a220eca83c0aaf
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-29 19:49:34 +0100

    Harpoon with keymap descriptor

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,62 +1,54 @@
+local desc = require('utils').pluginKeymapDescriptor('harpoon')
+
 return {
   'ThePrimeagen/harpoon',
   branch = 'harpoon2',
-  dependencies = {{'nvim-lua/plenary.nvim'}},
+  dependencies = {'nvim-lua/plenary.nvim'},
   config = function()
     local harpoon = require('harpoon')
     local extensions = require('harpoon.extensions')
-    local utils = require('utils')
-
     local menu_opts = {
       border = 'rounded',
       title_pos = 'center',
     }
 
-    local function km(rhs, command, opts)
-      vim.keymap.set(
-        'n',
-        rhs,
-        command,
-        utils.mergeTable(opts, {desc = opts.desc and 'Harpoon: ' .. opts.desc or nil})
-      )
-    end
-
     harpoon:setup({settings = {save_on_toggle = true, sync_on_ui_close = true}})
     harpoon:extend(extensions.builtins.command_on_nav('UfoEnableFold'))
     harpoon:extend({
       UI_CREATE = function(cx)
-        km(
+        vim.keymap.set(
+          'n',
           '<C-v>',
-          function()
-            harpoon.ui:select_menu_item({vsplit = true})
-          end,
-          {desc = 'Open as vertical split', buffer = cx.bufnr}
+          function() harpoon.ui:select_menu_item({vsplit = true}) end,
+          {desc = desc('Open as vertical split'), buffer = cx.bufnr}
         )
-        km(
+        vim.keymap.set(
+          'n',
           '<C-x>',
           function() harpoon.ui:select_menu_item({split = true}) end,
-          {desc = 'Open as split', buffer = cx.bufnr}
+          {desc = desc('Open as split'), buffer = cx.bufnr}
         )
-        km(
+        vim.keymap.set(
+          'n',
           '<C-t>',
           function() harpoon.ui:select_menu_item({tabedit = true}) end,
-          {desc = 'Open in new tab', buffer = cx.bufnr}
+          {desc = desc('Open in new tab'), buffer = cx.bufnr}
         )
       end,
     })
 
-    km(';a', function() harpoon:list():append() end, {desc = 'Add to list'})
-    km(';h', function() harpoon.ui:toggle_quick_menu(harpoon:list(), menu_opts) end, {desc = 'Open menu'})
-    km(';1', function() harpoon:list():select(1) end, {desc = 'Select file 1'})
-    km(';2', function() harpoon:list():select(2) end, {desc = 'Select file 2'})
-    km(';3', function() harpoon:list():select(3) end, {desc = 'Select file 3'})
-    km(';4', function() harpoon:list():select(4) end, {desc = 'Select file 4'})
-    km(';5', function() harpoon:list():select(5) end, {desc = 'Select file 5'})
-    km(';6', function() harpoon:list():select(6) end, {desc = 'Select file 6'})
-    km(';7', function() harpoon:list():select(7) end, {desc = 'Select file 7'})
-    km(';8', function() harpoon:list():select(8) end, {desc = 'Select file 8'})
-    km(';9', function() harpoon:list():select(9) end, {desc = 'Select file 9'}) -- If I ever have more than 9 tabs open, time to rethink my life choices
-    km('<S-h>', function() harpoon:list():prev() end, {desc = 'Previous file'})
-    km('<S-l>', function() harpoon:list():next() end, {desc = 'Next file'})
+    vim.keymap.set('n', ';h', function() harpoon.ui:toggle_quick_menu(harpoon:list(), menu_opts) end, {desc = desc('Open menu')})
+    vim.keymap.set('n', ';a', function() harpoon:list():append() end, {desc = desc('Add to list')})
+    vim.keymap.set('n', ';1', function() harpoon:list():select(1) end, {desc = desc('Select file 1')})
+    vim.keymap.set('n', ';2', function() harpoon:list():select(2) end, {desc = desc('Select file 2')})
+    vim.keymap.set('n', ';3', function() harpoon:list():select(3) end, {desc = desc('Select file 3')})
+    vim.keymap.set('n', ';4', function() harpoon:list():select(4) end, {desc = desc('Select file 4')})
+    vim.keymap.set('n', ';5', function() harpoon:list():select(5) end, {desc = desc('Select file 5')})
+    vim.keymap.set('n', ';6', function() harpoon:list():select(6) end, {desc = desc('Select file 6')})
+    vim.keymap.set('n', ';7', function() harpoon:list():select(7) end, {desc = desc('Select file 7')})
+    vim.keymap.set('n', ';8', function() harpoon:list():select(8) end, {desc = desc('Select file 8')})
+    vim.keymap.set('n', ';9', function() harpoon:list():select(9) end, {desc = desc('Select file 9')}) -- If I ever have more than 9 tabs open, time to rethink my life choices
+    vim.keymap.set('n', '<S-h>', function() harpoon:list():prev() end, {desc = desc('Previous file')})
+    vim.keymap.set('n', '<S-l>', function() harpoon:list():next() end, {desc = desc('Next file')})
   end
 }
