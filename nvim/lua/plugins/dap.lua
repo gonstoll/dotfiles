b120dba7e80a2a03d99402ee8da3cf285ef6e8f1
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2023-12-30 22:35:36 +0100

    Dap config update

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -21,30 +21,81 @@ local function get_args(config)
 end
 
 return {
+
   {
-    'rcarriga/nvim-dap-ui',
-    keys = {
-      {'<leader>du', function() require('dapui').toggle({}) end, desc = desc('Dap UI')},
-      {'<leader>de', function() require('dapui').eval() end, desc = desc('Eval'), mode = {'n', 'v'}},
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      {
+        'rcarriga/nvim-dap-ui',
+        keys = {
+          {'<leader>du', function() require('dapui').toggle({}) end, desc = desc('Dap UI')},
+          {'<leader>de', function() require('dapui').eval() end, desc = desc('Eval'), mode = {'n', 'v'}},
+        },
+        opts = {},
+      },
+      -- Install the vscode-js-debug adapter
+      {
+        'microsoft/vscode-js-debug',
+        -- After install, build it and rename the dist directory to out
+        build = 'npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out',
+        version = '1.*',
+      },
+      {
+        'mxsdev/nvim-dap-vscode-js',
+        opts = {
+          debugger_path = vim.fn.resolve(vim.fn.stdpath('data') .. '/lazy/vscode-js-debug'),
+        },
+      },
+      {
+        'theHamsta/nvim-dap-virtual-text',
+        opts = {},
+      },
+      -- {
+      --   'Joakker/lua-json5',
+      --   build = './install.sh',
+      -- },
     },
-    opts = {},
-    config = function(_, opts)
+    keys = {
+      {'<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = desc('Breakpoint Condition')},
+      {'<leader>db', function() require('dap').toggle_breakpoint() end, desc = desc('Toggle Breakpoint')},
+      {'<leader>dc', function() require('dap').continue() end, desc = desc('Continue')},
+      {'<leader>dC', function() require('dap').run_to_cursor() end, desc = desc('Run to Cursor')},
+      {'<leader>dg', function() require('dap').goto_() end, desc = desc('Go to line (no execute)')},
+      {'<leader>di', function() require('dap').step_into() end, desc = desc('Step Into')},
+      {'<leader>dj', function() require('dap').down() end, desc = desc('Down')},
+      {'<leader>dk', function() require('dap').up() end, desc = desc('Up')},
+      {'<leader>dl', function() require('dap').run_last() end, desc = desc('Run Last')},
+      {'<leader>do', function() require('dap').step_out() end, desc = desc('Step Out')},
+      {'<leader>dO', function() require('dap').step_over() end, desc = desc('Step Over')},
+      {'<leader>dp', function() require('dap').pause() end, desc = desc('Pause')},
+      {'<leader>dr', function() require('dap').repl.toggle() end, desc = desc('Toggle REPL')},
+      {'<leader>ds', function() require('dap').session() end, desc = desc('Session')},
+      {'<leader>dt', function() require('dap').terminate() end, desc = desc('Terminate')},
+      {'<leader>dw', function() require('dap.ui.widgets').hover() end, desc = desc('Widgets')},
+      {
+        '<leader>da',
+        function()
+          if vim.fn.filereadable('.vscode/launch.json') then
+            local dap_vscode = require('dap.ext.vscode')
+            dap_vscode.load_launchjs(nil, {
+              ['pwa-node'] = js_based_languages,
+              ['chrome'] = js_based_languages,
+              ['pwa-chrome'] = js_based_languages,
+            })
+          end
+          require('dap').continue({before = get_args})
+        end,
+        desc = desc('Run with Args'),
+      },
+    },
+    config = function()
       local dap = require('dap')
       local dapui = require('dapui')
-
-      dapui.setup(opts)
+      local icons = require('utils.icons')
 
       dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open({}) end
       dap.listeners.before.event_terminated['dapui_config'] = function() dapui.close({}) end
       dap.listeners.before.event_exited['dapui_config'] = function() dapui.close({}) end
-    end,
-  },
-
-  {
-    'mfussenegger/nvim-dap',
-    config = function()
-      local dap = require('dap')
-      local icons = require('utils.icons')
 
       vim.api.nvim_set_hl(0, 'DapStoppedLine', {default = true, link = 'Visual'})
 
@@ -110,61 +161,5 @@ return {
         }
       end
     end,
-    keys = {
-      {'<leader>dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, desc = desc('Breakpoint Condition')},
-      {'<leader>db', function() require('dap').toggle_breakpoint() end, desc = desc('Toggle Breakpoint')},
-      {'<leader>dc', function() require('dap').continue() end, desc = desc('Continue')},
-      {'<leader>dC', function() require('dap').run_to_cursor() end, desc = desc('Run to Cursor')},
-      {'<leader>dg', function() require('dap').goto_() end, desc = desc('Go to line (no execute)')},
-      {'<leader>di', function() require('dap').step_into() end, desc = desc('Step Into')},
-      {'<leader>dj', function() require('dap').down() end, desc = desc('Down')},
-      {'<leader>dk', function() require('dap').up() end, desc = desc('Up')},
-      {'<leader>dl', function() require('dap').run_last() end, desc = desc('Run Last')},
-      {'<leader>do', function() require('dap').step_out() end, desc = desc('Step Out')},
-      {'<leader>dO', function() require('dap').step_over() end, desc = desc('Step Over')},
-      {'<leader>dp', function() require('dap').pause() end, desc = desc('Pause')},
-      {'<leader>dr', function() require('dap').repl.toggle() end, desc = desc('Toggle REPL')},
-      {'<leader>ds', function() require('dap').session() end, desc = desc('Session')},
-      {'<leader>dt', function() require('dap').terminate() end, desc = desc('Terminate')},
-      {'<leader>dw', function() require('dap.ui.widgets').hover() end, desc = desc('Widgets')},
-      {
-        '<leader>da',
-        function()
-          if vim.fn.filereadable('.vscode/launch.json') then
-            local dap_vscode = require('dap.ext.vscode')
-            dap_vscode.load_launchjs(nil, {
-              ['pwa-node'] = js_based_languages,
-              ['chrome'] = js_based_languages,
-              ['pwa-chrome'] = js_based_languages,
-            })
-          end
-          require('dap').continue({before = get_args})
-        end,
-        desc = desc('Run with Args'),
-      },
-    },
-    dependencies = {
-      -- Install the vscode-js-debug adapter
-      {
-        'microsoft/vscode-js-debug',
-        -- After install, build it and rename the dist directory to out
-        build = 'npm install --legacy-peer-deps --no-save && npx gulp vsDebugServerBundle && rm -rf out && mv dist out',
-        version = '1.*',
-      },
-      {
-        'mxsdev/nvim-dap-vscode-js',
-        opts = {
-          debugger_path = vim.fn.resolve(vim.fn.stdpath('data') .. '/lazy/vscode-js-debug'),
-        },
-      },
-      {
-        'theHamsta/nvim-dap-virtual-text',
-        opts = {},
-      },
-      -- {
-      --   'Joakker/lua-json5',
-      --   build = './install.sh',
-      -- },
-    },
   },
 }
