2554823ff5c2ab21574dfb683d309348f6e97e75
Gonzalo Stoll <gonzalostoll@gonzalo.local>
2024-01-07 20:06:13 +0100

    Starting with luasnip snippets

---
+++ nvim/after/plugin/luasnip.lua
@@ -0,0 +1,66 @@
+local desc = require('utils').plugin_keymap_desc('luasnip')
+
+local ls = require('luasnip')
+local types = require('luasnip.util.types')
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
+
+ls.config.set_config({
+  history = true,
+  updateevents = 'TextChanged,TextChangedI',
+  enable_autosnippets = true,
+  ext_opts = {
+    [types.choiceNode] = {
+      active = {
+        virt_text = {{'<-', 'Current choice'}},
+      }
+    },
+  },
+})
+
+local t = ls.text_node
+local i = ls.insert_node
+local c = ls.choice_node
+local f = ls.function_node
+local d = ls.dynamic_node
+local snippet = ls.s
+local snippet_from_nodes = ls.sn
+
+-- Keymaps
+vim.keymap.set('n', '<leader><leader>s', '<cmd>source ~/.config/nvim/after/plugin/luasnip.lua<CR>', {
+  desc = desc('Source snippets')
+})
+vim.keymap.set({'i', 's'}, '<C-l>', function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, {desc = desc('Next choice')})
+
+-- Snippets
+ls.add_snippets('all', {
+  snippet('date', {
+    f(function()
+      return string.format(string.gsub(vim.bo.commentstring, '%%s', ' %%s'), os.date())
+    end, {}),
+  }),
+  snippet('todo', {
+    c(1, {
+      t('TODO: (gonza) - '),
+      t('NOTE: (gonza) - '),
+      t('FIX: (gonza) - '),
+      t('WARNING: (gonza) - '),
+      t('HACK: (gonza) - '),
+      t('PERF: (gonza) - '),
+    })
+  })
+})
+
+ls.add_snippets('lua', {
+  snippet('req', fmt("local {} = require('{}')", {
+    f(function(import_name)
+      local parts = vim.split(import_name[1][1], '.', true)
+      return parts[#parts] or ''
+    end, {1}),
+    i(1, 'module')
+  }))
+})
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -86,7 +86,7 @@ return {
         {name = 'path'},
       }),
       formatting = {
-        fields = {'menu', 'abbr', 'kind'},
+        fields = {'abbr', 'menu', 'kind'},
         format = lspkind.cmp_format({
           mode = 'symbol',
           maxwidth = 200,
@@ -111,17 +111,17 @@ return {
           end
         })
       },
-      enabled = function()
-        -- disable completion in comments
-        local context = require('cmp.config.context')
-
-        -- keep command mode completion enabled when cursor is in a comment
-        if vim.api.nvim_get_mode().mode == 'c' then
-          return true
-        else
-          return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
-        end
-      end,
+      -- -- disable completion in comments
+      -- enabled = function()
+      --   local context = require('cmp.config.context')
+      --
+      --   -- keep command mode completion enabled when cursor is in a comment
+      --   if vim.api.nvim_get_mode().mode == 'c' then
+      --     return true
+      --   else
+      --     return not context.in_treesitter_capture('comment') and not context.in_syntax_group('Comment')
+      --   end
+      -- end,
     })
 
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
