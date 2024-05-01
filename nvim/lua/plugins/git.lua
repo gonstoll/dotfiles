f09c64cde207c91b815c2afb87ff3ff60c276044
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-05-01 14:42:33 +0200

    Centering after hunk lookup

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -57,13 +57,19 @@ return {
           -- Navigation
           map('n', ']c', function()
             if vim.wo.diff then return ']c' end
-            vim.schedule(function() gs.next_hunk() end)
+            vim.schedule(function()
+              gs.next_hunk()
+              vim.cmd('normal! zz')
+            end)
             return '<Ignore>'
           end, {expr = true, desc = desc('Next hunk')})
 
           map('n', '[c', function()
             if vim.wo.diff then return '[c' end
-            vim.schedule(function() gs.prev_hunk() end)
+            vim.schedule(function()
+              gs.prev_hunk()
+              vim.cmd('normal! zz')
+            end)
             return '<Ignore>'
           end, {expr = true, desc = desc('Previous hunk')})
 
