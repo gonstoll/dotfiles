58c3485e4dcf86f9bc797f4375cf5e528b5df213
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-06-03 15:48:40 +0200

    Some changes on neogen

--- nvim/lua/plugins/neogen.lua
+++ nvim/lua/plugins/neogen.lua
@@ -3,27 +3,14 @@ local desc = require('utils').plugin_keymap_desc('neogen')
 return {
   'danymat/neogen',
   dependencies = 'nvim-treesitter/nvim-treesitter',
-  keys = {
-    {
-      '<leader>nf',
-      function() require('neogen').generate({type = 'func'}) end,
-      desc = desc('Generate function docs'),
-    },
-    {
-      '<leader>nt',
-      function() require('neogen').generate({type = 'type'}) end,
-      desc = desc('Generate type docs'),
-    },
-    {
-      '<leader>nc',
-      function() require('neogen').generate({type = 'class'}) end,
-      desc = desc('Generate class docs'),
-    },
-    {
-      '<leader>ne',
-      function() require('neogen').generate({type = 'file'}) end,
-      desc = desc('Generate file docs'),
-    },
-  },
+  keys = function()
+    local neogen = require('neogen')
+    return {
+      {'<leader>nf', function() neogen.generate({type = 'func'}) end, desc = desc('Generate function docs')},
+      {'<leader>nt', function() neogen.generate({type = 'type'}) end, desc = desc('Generate type docs')},
+      {'<leader>nc', function() neogen.generate({type = 'class'}) end, desc = desc('Generate class docs')},
+      {'<leader>ne', function() neogen.generate({type = 'file'}) end, desc = desc('Generate file docs')}
+    }
+  end,
   opts = {snippet_engine = 'luasnip'},
 }
