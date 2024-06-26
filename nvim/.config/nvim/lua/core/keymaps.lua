bb9e6d2762ea7364623003cae671b6d076b85ca9
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-06-26 18:36:30 +0200

    keyset

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -1,58 +1,55 @@
+local keyset = vim.keymap.set
+
 -- Select all
-vim.keymap.set('n', '<C-b>', 'gg<S-v>G', {desc = 'Select all'})
+keyset('n', '<C-b>', 'gg<S-v>G', {desc = 'Select all'})
 
 -- New tab
-vim.keymap.set('n', 'tn', ':tabnew %<CR>', {desc = 'New tab'})
-vim.keymap.set('n', 'tc', ':tabclose<CR>', {desc = 'Close tab'})
+keyset('n', 'tn', ':tabnew %<CR>', {desc = 'New tab'})
+keyset('n', 'tc', ':tabclose<CR>', {desc = 'Close tab'})
 -- Split window
-vim.keymap.set('n', 'ss', ':split<Return><C-w>w', {desc = 'Split window horizontally'}) -- Horizontal
-vim.keymap.set('n', 'sv', ':vsplit<Return><C-w>w', {desc = 'Split window vertically'})  -- Vertical
--- Move window
--- vim.keymap.set('', '<C-h>', '<C-w>h', {desc = 'Move window (left)'})                    -- Left
--- vim.keymap.set('', '<C-k>', '<C-w>k', {desc = 'Move window (up)'})                      -- Up
--- vim.keymap.set('', '<C-j>', '<C-w>j', {desc = 'Move window (down)'})                    -- Down
--- vim.keymap.set('', '<C-l>', '<C-w>l', {desc = 'Move window (right)'})                   -- Right
+keyset('n', 'ss', ':split<Return><C-w>w', {desc = 'Split window horizontally'}) -- Horizontal
+keyset('n', 'sv', ':vsplit<Return><C-w>w', {desc = 'Split window vertically'})  -- Vertical
 
-vim.keymap.set('n', '<C-p>', '<cmd>cprev<CR>zz', {desc = 'Previous quickfix item'})
-vim.keymap.set('n', '<C-n>', '<cmd>cnext<CR>zz', {desc = 'Next quickfix item'})
+-- Move between qf items
+keyset('n', '<C-p>', '<cmd>cprev<CR>zz', {desc = 'Previous quickfix item'})
+keyset('n', '<C-n>', '<cmd>cnext<CR>zz', {desc = 'Next quickfix item'})
 
 -- Resize splits
-vim.keymap.set('n', '<M-h>', '<C-w>5>', {desc = 'Resize window (left)'})
-vim.keymap.set('n', '<M-l>', '<C-w>5<', {desc = 'Resize window (right)'})
-vim.keymap.set('n', '<M-j>', '<C-w>5-', {desc = 'Resize window (down)'})
-vim.keymap.set('n', '<M-k>', '<C-w>5+', {desc = 'Resize window (up)'})
+keyset('n', '<M-h>', '<C-w>5>', {desc = 'Resize window (left)'})
+keyset('n', '<M-l>', '<C-w>5<', {desc = 'Resize window (right)'})
+keyset('n', '<M-j>', '<C-w>5-', {desc = 'Resize window (down)'})
+keyset('n', '<M-k>', '<C-w>5+', {desc = 'Resize window (up)'})
 
 -- Move lines
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", {desc = 'Move line down'})
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", {desc = 'Move line up'})
-
-vim.keymap.set('n', 'J', 'mzJ`z')
+keyset('v', 'J', ":m '>+1<CR>gv=gv", {desc = 'Move line down'})
+keyset('v', 'K', ":m '<-2<CR>gv=gv", {desc = 'Move line up'})
+keyset('n', 'J', 'mzJ`z')
 
 -- Jumping pages keeps cursor in the middle
-vim.keymap.set('n', '<C-d>', '<C-d>zz', {desc = 'Jump page down'})
-vim.keymap.set('n', '<C-u>', '<C-u>zz', {desc = 'Jump page up'})
+keyset('n', '<C-d>', '<C-d>zz', {desc = 'Jump page down'})
+keyset('n', '<C-u>', '<C-u>zz', {desc = 'Jump page up'})
 
 -- Keep search terms in the middle of the screen
-vim.keymap.set('n', 'n', 'nzzzv', {desc = 'Jump to next search term'})
-vim.keymap.set('n', 'N', 'Nzzzv', {desc = 'Jump to previous search term'})
+keyset('n', 'n', 'nzzzv', {desc = 'Jump to next search term'})
+keyset('n', 'N', 'Nzzzv', {desc = 'Jump to previous search term'})
 
 -- Pastes copied buffer and keeps it in the register
-vim.keymap.set('x', '<leader>p', '\"_dP')
+keyset('x', '<leader>p', '\"_dP')
 
 -- Sources current buffer
-vim.keymap.set('n', '<leader><leader>', function() vim.cmd('so') end)
+keyset('n', '<leader><leader>', function() vim.cmd('so') end)
 
 -- Opens fugitive
-vim.keymap.set('n', '<leader>gs', ':top Git<CR>', {desc = 'Fugitive: Open git console'})
+keyset('n', '<leader>gs', ':top Git<CR>', {desc = 'Fugitive: Open git console'})
 
 -- Toggle highlighting search
-vim.keymap.set('n', '<leader>;h', ':set hlsearch!<CR>', {desc = 'Toggle highlighting search'})
+keyset('n', '<leader>;h', ':set hlsearch!<CR>', {desc = 'Toggle highlighting search'})
 
 -- Save without formatting
-vim.keymap.set('n', ';wf', ':noautocmd w<CR>', {desc = 'Save without formatting'})
+keyset('n', ';wf', ':noautocmd w<CR>', {desc = 'Save without formatting'})
 
 -- Open terminal below
-vim.keymap.set('n', ',st', function()
+keyset('n', ',st', function()
   vim.cmd.new()
   vim.cmd.wincmd('J')
   vim.api.nvim_win_set_height(0, 12)
