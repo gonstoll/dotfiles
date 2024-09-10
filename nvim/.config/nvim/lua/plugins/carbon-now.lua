edc77b5f48ead3e2697e39edb7a014daded3c178
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-09-10 23:34:06 +0200

    Removing plugin (carbon-now)

--- nvim/.config/nvim/lua/plugins/carbon-now.lua
+++
@@ -1,8 +0,0 @@
-local desc = require('utils').plugin_keymap_desc('carbon now')
-
-return {
-  'kristijanhusak/vim-carbon-now-sh',
-  keys = {
-    {'<F2>', ':CarbonNowSh<CR>', desc = desc('Take code snapshot'), mode = {'v', 'n'}},
-  },
-}
