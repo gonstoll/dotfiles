d53cb1c0b021858086ac2b7f7153fa8018eb7ca1
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-09-17 22:13:17 +0200

    Removing ts/js autocommands - lua dap

--- nvim/.config/nvim/lua/plugins/dap.lua
+++ nvim/.config/nvim/lua/plugins/dap.lua
@@ -229,16 +229,49 @@ return {
     end
 
     -- Lua configurations.
+    -- 1. Open a Neovim instance (instance A)
+    -- 2. Launch the DAP server with (A) >
+    --    :lua require"osv".launch({port=8086})
+    -- 3. Open another Neovim instance (instance B)
+    -- 4. Open `myscript.lua` (B)
+    -- 5. Place a breakpoint on line 2 using (B) >
+    --    :lua require"dap".toggle_breakpoint()
+    -- 6. Connect the DAP client using (B) >
+    --    :lua require"dap".continue()
+    -- 7. Run `myscript.lua` in the other instance (A) >
+    --    :luafile myscript.lua
+    -- 8. The breakpoint should hit and freeze the instance (B)
+
     dap.adapters.nlua = function(callback, config)
-      callback({type = 'server', host = config.host or '127.0.0.1', port = config.port or 8086})
+      local adapter = {
+        type = 'server',
+        host = config.host or '127.0.0.1',
+        port = config.port or 8086,
+      }
+      if config.start_neovim then
+        local dap_run = dap.run
+        dap.run = function(c)
+          adapter.port = c.port
+          adapter.host = c.host
+        end
+        require('osv').run_this()
+        dap.run = dap_run
+      end
+      callback(adapter)
     end
 
-    dap.configurations['lua'] = {
+    dap.configurations.lua = {
       {
         type = 'nlua',
         request = 'attach',
-        name = 'Attach to running Neovim instance',
-        host = '127.0.0.1',
+        name = 'Run this file',
+        start_neovim = {},
+      },
+      {
+        type = 'nlua',
+        request = 'attach',
+        name = 'Attach to running Neovim instance (port = 8086)',
+        port = 8086,
       },
     }
   end,
