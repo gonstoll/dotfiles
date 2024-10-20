dc632b7ce5e60d954f55c966d23f84326ab54c8e
Gonzalo Stoll <stollgonzalo@gmail.com>
2024-10-20 18:44:32 +0200

    yioneko's fork 🛬

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -4,7 +4,6 @@ return {
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-nvim-lsp',
-    'rafamadriz/friendly-snippets',
     'hrsh7th/cmp-buffer',
     'hrsh7th/cmp-path',
     'hrsh7th/cmp-cmdline',
