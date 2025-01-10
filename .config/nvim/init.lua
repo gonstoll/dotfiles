af3adfce14f95fa3a3fe25af5dc41142aefdfd05
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-01-10 20:16:10 +0100

    Temporary fix for :Inspect

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,6 @@
 -- https://github.com/neovim/neovim/issues/17070#issuecomment-1086775760
 -- vim.loop.fs_write(2, '\27Ptmux;\27\27]11;?\7\27\\', -1, nil)
 _G.Utils = require('utils')
+-- https://github.com/neovim/neovim/issues/31675
+vim.hl = vim.highlight
 require('core')
