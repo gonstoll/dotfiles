d33e426e046a1a0da8759e461a91f668c23310ca
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-01-22 10:42:32 +0100

    Some go settings

--- .config/nvim/lua/plugins/lsp/servers/gopls.lua
+++ .config/nvim/lua/plugins/lsp/servers/gopls.lua
@@ -1,3 +1,27 @@
+-- Organize go imports (https://github.com/harrisoncramer/nvim/blob/main/lua/lsp/servers/gopls.lua)
+-- local function goimports()
+--   local params = vim.lsp.util.make_range_params()
+--   params.context = {only = {'source.organizeImports'}}
+--   local result = vim.lsp.buf_request_sync(0, 'textDocument/codeAction', params)
+--   for cid, res in pairs(result or {}) do
+--     for _, r in pairs(res.result or {}) do
+--       if r.edit then
+--         local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or 'utf-16'
+--         vim.lsp.util.apply_workspace_edit(r.edit, enc)
+--       end
+--     end
+--   end
+--   vim.lsp.buf.format({async = false})
+-- end
+--
+-- vim.api.nvim_create_autocmd('BufWritePre', {
+--   pattern = '*.go',
+--   callback = function()
+--     goimports()
+--   end,
+--   desc = 'Run goimports on save in Golang files',
+-- })
+
 return {
   settings = {
     gopls = {
