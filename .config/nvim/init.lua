86e8f69e0114693f367ee1d7e8bb9fe16d46b23d
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-01-28 23:43:07 +0100

    Python formatter (black)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,10 @@
 -- Add `t_RB` so that neovim detect background
 -- https://github.com/neovim/neovim/issues/17070#issuecomment-1086775760
 -- vim.loop.fs_write(2, '\27Ptmux;\27\27]11;?\7\27\\', -1, nil)
+
 _G.Utils = require('utils')
+
 -- https://github.com/neovim/neovim/issues/31675
 vim.hl = vim.highlight
+
 require('core')
--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,48 +1,48 @@
 return {
-  {
-    'github/copilot.vim',
-    config = false,
-    cmd = 'Copilot',
-    build = ':Copilot auth',
-    init = function()
-      vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', {
-        noremap = true,
-        silent = true,
-        expr = true,
-        replace_keycodes = false,
-      })
+    {
+        'github/copilot.vim',
+        config = false,
+        cmd = 'Copilot',
+        build = ':Copilot auth',
+        init = function()
+            vim.keymap.set('i', '<C-j>', 'copilot#Accept("<CR>")', {
+                noremap = true,
+                silent = true,
+                expr = true,
+                replace_keycodes = false,
+            })
 
-      vim.g.copilot_no_tab_map = true
-      vim.g.copilot_assume_mapped = true
-    end,
-  },
+            vim.g.copilot_no_tab_map = true
+            vim.g.copilot_assume_mapped = true
+        end,
+    },
 
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    cmd = {
-      'CopilotChat',
-      'CopilotChatFix',
-      'CopilotChatDocs',
-      'CopilotChatLoad',
-      'CopilotChatOpen',
-      'CopilotChatSave',
-      'CopilotChatClose',
-      'CopilotChatToggle',
-      'CopilotChatStop',
-      'CopilotChatModel',
-      'CopilotChatModels',
-      'CopilotChatReset',
-      'CopilotChatCommit',
-      'CopilotChatTests',
-      'CopilotChatReview',
-      'CopilotChatExplain',
-      'CopilotChatDebugInfo',
-      'CopilotChatCommitStaged',
-      'CopilotChatFixDiagnostic',
+    {
+        'CopilotC-Nvim/CopilotChat.nvim',
+        cmd = {
+            'CopilotChat',
+            'CopilotChatFix',
+            'CopilotChatDocs',
+            'CopilotChatLoad',
+            'CopilotChatOpen',
+            'CopilotChatSave',
+            'CopilotChatClose',
+            'CopilotChatToggle',
+            'CopilotChatStop',
+            'CopilotChatModel',
+            'CopilotChatModels',
+            'CopilotChatReset',
+            'CopilotChatCommit',
+            'CopilotChatTests',
+            'CopilotChatReview',
+            'CopilotChatExplain',
+            'CopilotChatDebugInfo',
+            'CopilotChatCommitStaged',
+            'CopilotChatFixDiagnostic',
+        },
+        dependencies = {'github/copilot.vim', 'nvim-lua/plenary.nvim'},
+        opts = {
+            debug = false,
+        },
     },
-    dependencies = {'github/copilot.vim', 'nvim-lua/plenary.nvim'},
-    opts = {
-      debug = false,
-    },
-  },
 }
