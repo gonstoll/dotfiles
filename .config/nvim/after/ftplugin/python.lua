ec28cc0c88287b17e51dfb8f67d0910c15111ac4
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-01-30 15:59:13 +0100

    Terminal height when runnning python

--- .config/nvim/after/ftplugin/python.lua
+++ .config/nvim/after/ftplugin/python.lua
@@ -3,12 +3,12 @@ vim.opt.shiftwidth = 4
 
 vim.keymap.set('n', '<leader>pr', function()
     local filepath = vim.fn.expand('%')
-    local cmd = 'python3 ' .. filepath
+    local py_cmd = 'python3 ' .. filepath
+    local cmd = 'sh -c "' .. py_cmd .. '; exec $SHELL"'
     vim.cmd.new()
     vim.cmd.wincmd('J') -- Move to the window below
-    vim.api.nvim_win_set_height(0, 12)
+    vim.api.nvim_win_set_height(0, 14)
     vim.wo.winfixheight = true
-    vim.cmd.term(cmd)
     -- Open terminal window, execute file and keep terminal open
-    -- vim.cmd.term('sh -c "' .. cmd .. '; exec $SHELL"')
-end, {desc = ''})
+    vim.cmd.term(cmd)
+end, {desc = 'Python: Run current file'})
--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,78 +1,79 @@
 local desc = Utils.plugin_keymap_desc('snacks')
-local root_path = os.getenv('HOME') .. '/notes/scratch'
+local scratch_path = os.getenv('HOME') .. '/notes/scratch'
 
 return {
-  'folke/snacks.nvim',
-  lazy = false,
-  opts = {
-    bigfile = {enabled = true},
-    quickfile = {enabled = true},
-    gitbrowse = {enabled = true},
-    dashboard = {enabled = false},
-    notifier = {enabled = false},
-    statuscolumn = {enabled = false},
-    words = {enabled = false},
-    scratch = {
-      root = root_path,
-      win = {width = 150, height = 40, border = 'single'},
+    'folke/snacks.nvim',
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+        bigfile = {enabled = true},
+        quickfile = {enabled = true},
+        gitbrowse = {enabled = true},
+        dashboard = {enabled = false},
+        notifier = {enabled = false},
+        statuscolumn = {enabled = false},
+        words = {enabled = false},
+        scratch = {
+            root = scratch_path,
+            win = {width = 150, height = 40, border = 'single'},
+        },
     },
-  },
-  keys = function()
-    local snacks = require('snacks')
-    return {
-      {
-        '<leader>.',
-        function()
-          vim.ui.input({
-            prompt = 'Enter scratch buffer title: ',
-            default = '',
-          }, function(t)
-            if not vim.fn.isdirectory(root_path) then
-              vim.fn.mkdir(root_path, 'p')
-            end
+    keys = function()
+        local snacks = require('snacks')
+        return {
+            {
+                '<leader>.',
+                function()
+                    vim.ui.input({
+                        prompt = 'Enter scratch buffer title: ',
+                        default = '',
+                    }, function(t)
+                        if not vim.fn.isdirectory(scratch_path) then
+                            vim.fn.mkdir(scratch_path, 'p')
+                        end
 
-            local title = t ~= '' and t:gsub('%s+', '_') or 'Untitled'
-            snacks.scratch.open({
-              ft = 'markdown',
-              name = title .. '_' .. os.date('%Y-%m-%d-%H-%M-%S'),
-              win = {
-                title = title,
-              },
-            })
-          end)
-        end,
-        desc = desc('Open a scratch buffer'),
-      },
-      {
-        '<leader>S',
-        -- function() snacks.scratch.select() end,
-        function() Utils.fzf.scratch_select() end,
-        desc = desc('Select a scratch buffer'),
-      },
-      {
-        '<leader>cR',
-        function() snacks.rename.rename_file() end,
-        desc = desc('Rename File'),
-      },
-      {
-        '<leader>gy',
-        function() snacks.gitbrowse() end,
-        desc = desc('Open line(s) in browser'),
-        mode = {'n', 'v'},
-      },
-      {
-        '<leader>gY',
-        function()
-          snacks.gitbrowse.open({
-            open = function(url)
-              vim.fn.setreg('+', url)
-              vim.notify('Yanked url to clipboard')
-            end,
-          })
-        end,
-        desc = desc('Copy line(s) link'),
-        mode = {'n', 'v'},
-      },
-    }
-  end,
+                        local title = t ~= '' and t:gsub('%s+', '_') or 'Untitled'
+                        snacks.scratch.open({
+                            ft = 'markdown',
+                            name = title .. '_' .. os.date('%Y-%m-%d-%H-%M-%S'),
+                            win = {
+                                title = title,
+                            },
+                        })
+                    end)
+                end,
+                desc = desc('Open a scratch buffer'),
+            },
+            {
+                '<leader>S',
+                -- function() snacks.scratch.select() end,
+                function() Utils.fzf.scratch_select() end,
+                desc = desc('Select a scratch buffer'),
+            },
+            {
+                '<leader>cR',
+                function() snacks.rename.rename_file() end,
+                desc = desc('Rename File'),
+            },
+            {
+                '<leader>gy',
+                function() snacks.gitbrowse() end,
+                desc = desc('Open line(s) in browser'),
+                mode = {'n', 'v'},
+            },
+            {
+                '<leader>gY',
+                function()
+                    snacks.gitbrowse.open({
+                        open = function(url)
+                            vim.fn.setreg('+', url)
+                            vim.notify('Yanked url to clipboard')
+                        end,
+                    })
+                end,
+                desc = desc('Copy line(s) link'),
+                mode = {'n', 'v'},
+            },
+        }
+    end,
 }
