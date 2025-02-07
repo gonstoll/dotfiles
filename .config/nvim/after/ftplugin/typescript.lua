b1cb5b234cd676e5fdda81e1ab18d6d2879f39fc
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-02-07 14:57:29 +0100

    Sourcing other js/ts filetype files

--- .config/nvim/after/ftplugin/typescript.lua
+++ .config/nvim/after/ftplugin/typescript.lua
@@ -1,18 +1,1 @@
-local ts_utils = require("nvim-treesitter.ts_utils")
-
----@param key string
-local function newline_comment(key)
-    local node = ts_utils.get_node_at_cursor()
-    if not node then
-        return key
-    end
-    local line = vim.api.nvim_get_current_line():gsub("^%s+", "")
-    if string.find(line, "^/?%*") then
-        return key .. "*"
-    end
-    return key
-end
-
-vim.keymap.set("n", "o", function() return newline_comment("o") end, {buffer = true, expr = true})
-vim.keymap.set("n", "O", function() return newline_comment("O") end, {buffer = true, expr = true})
-vim.keymap.set("i", "<cr>", function() return newline_comment("<cr>") end, {buffer = true, expr = true})
+vim.cmd.source(vim.fn.stdpath("config") .. "/after/ftplugin/javascript.lua")
--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -2,40 +2,40 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight yanked text
-autocmd('TextYankPost', {
-    desc = 'Highlight yanked text',
-    group = augroup('YankHighlight', {}),
+autocmd("TextYankPost", {
+    desc = "Highlight yanked text",
+    group = augroup("YankHighlight", {}),
     callback = function()
         vim.highlight.on_yank()
     end,
 })
 
 -- Disable eslint on node_modules
-autocmd({'BufNewFile', 'BufRead'}, {
-    pattern = {'**/node_modules/**', 'node_modules', '/node_modules/*'},
-    group = augroup('DisableEslintOnNodeModules', {}),
+autocmd({"BufNewFile", "BufRead"}, {
+    pattern = {"**/node_modules/**", "node_modules", "/node_modules/*"},
+    group = augroup("DisableEslintOnNodeModules", {}),
     callback = function()
         vim.diagnostic.disable(0)
     end,
 })
 
 -- Fugitive keymaps
-autocmd('BufWinEnter', {
-    pattern = 'fugitive',
-    group = augroup('Fugitive', {}),
+autocmd("BufWinEnter", {
+    pattern = "fugitive",
+    group = augroup("Fugitive", {}),
     callback = function()
-        if (vim.bo.filetype ~= 'fugitive') then
+        if (vim.bo.filetype ~= "fugitive") then
             return
         end
 
         -- Only run this on fugitive buffers
         local bufnr = vim.api.nvim_get_current_buf()
-        vim.keymap.set('n', '<leader>P', function()
-            vim.cmd.Git('push')
-        end, {buffer = bufnr, remap = false, desc = 'Fugitive: Push'})
-        vim.keymap.set('n', '<leader>p', function()
-            vim.cmd.Git('pull')
-        end, {buffer = bufnr, remap = false, desc = 'Fugitive: Pull'})
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git("push")
+        end, {buffer = bufnr, remap = false, desc = "Fugitive: Push"})
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git("pull")
+        end, {buffer = bufnr, remap = false, desc = "Fugitive: Pull"})
     end,
 })
 
@@ -43,12 +43,12 @@ autocmd('BufWinEnter', {
 -- vim.cmd('autocmd BufEnter * :lua require("lazygit.utils").project_root_dir()')
 
 -- Statusline
-local statusline_group = augroup('StatusLine', {})
-autocmd({'WinEnter', 'BufEnter'}, {
-    pattern = '*',
+local statusline_group = augroup("StatusLine", {})
+autocmd({"WinEnter", "BufEnter"}, {
+    pattern = "*",
     group = statusline_group,
     callback = function()
-        if (vim.bo.filetype == 'oil') then
+        if (vim.bo.filetype == "oil") then
             vim.o.statusline = "%!v:lua.require('statusline').oil()"
             return
         end
@@ -56,25 +56,25 @@ autocmd({'WinEnter', 'BufEnter'}, {
     end,
 })
 
-autocmd({'WinLeave', 'BufLeave'}, {
-    pattern = '*',
+autocmd({"WinLeave", "BufLeave"}, {
+    pattern = "*",
     group = statusline_group,
     callback = function()
         vim.o.statusline = "%!v:lua.require('statusline').inactive()"
     end,
 })
 
-autocmd('ColorScheme', {
-    group = augroup('cursor-highlight', {}),
+autocmd("ColorScheme", {
+    group = augroup("cursor-highlight", {}),
     callback = function()
-        if vim.o.background == 'light' then
-            vim.cmd('highlight! clear Cursor')
+        if vim.o.background == "light" then
+            vim.cmd("highlight! clear Cursor")
         end
     end,
 })
 
-autocmd('TermOpen', {
-    group = augroup('custom-term-open', {}),
+autocmd("TermOpen", {
+    group = augroup("custom-term-open", {}),
     callback = function()
         vim.opt_local.number = false
         vim.opt_local.relativenumber = false
@@ -82,52 +82,52 @@ autocmd('TermOpen', {
     end,
 })
 
-autocmd('FileType', {
-    pattern = '*',
-    group = augroup('diable-new-line-comments', {}),
+autocmd("FileType", {
+    pattern = "*",
+    group = augroup("diable-new-line-comments", {}),
     callback = function()
-        vim.opt_local.formatoptions:remove('o')
-        vim.opt_local.formatoptions:remove('r')
-        vim.opt_local.formatoptions:remove('c')
+        vim.opt_local.formatoptions:remove("o")
+        vim.opt_local.formatoptions:remove("r")
+        vim.opt_local.formatoptions:remove("c")
     end,
 })
 
-autocmd('BufWritePost', {
-    pattern = 'aerospace.toml',
-    group = augroup('Aerospace', {}),
-    command = '!aerospace reload-config',
+autocmd("BufWritePost", {
+    pattern = "aerospace.toml",
+    group = augroup("Aerospace", {}),
+    command = "!aerospace reload-config",
 })
 
-autocmd('BufWritePost', {
-    pattern = '*',
-    group = augroup('FileDetect', {}),
-    desc = 'Detect filetype on files with on extension after saving the file',
+autocmd("BufWritePost", {
+    pattern = "*",
+    group = augroup("FileDetect", {}),
+    desc = "Detect filetype on files with on extension after saving the file",
     callback = function()
-        if vim.bo.filetype == '' then
-            vim.cmd('filetype detect')
+        if vim.bo.filetype == "" then
+            vim.cmd("filetype detect")
         end
     end,
 })
 
 -- Redir
-vim.api.nvim_create_user_command('Redir', Utils.redir.redir, {
-    nargs = '+',
-    complete = 'command',
+vim.api.nvim_create_user_command("Redir", Utils.redir.redir, {
+    nargs = "+",
+    complete = "command",
     range = true,
     bang = true,
 })
 
-vim.api.nvim_create_user_command('EvalFile', function(args)
+vim.api.nvim_create_user_command("EvalFile", function(args)
     local bang = args.bang
-    Utils.redir.evaler('%')(bang)
+    Utils.redir.evaler("%")(bang)
 end, {bar = true, bang = true})
 
-vim.api.nvim_create_user_command('EvalLine', function(args)
+vim.api.nvim_create_user_command("EvalLine", function(args)
     local bang = args.bang
-    Utils.redir.evaler('.')(bang)
+    Utils.redir.evaler(".")(bang)
 end, {bar = true, bang = true})
 
-vim.api.nvim_create_user_command('EvalRange', function(args)
+vim.api.nvim_create_user_command("EvalRange", function(args)
     local bang = args.bang
     Utils.redir.evaler("'<,'>")(bang)
 end, {bar = true, bang = true, range = true})
