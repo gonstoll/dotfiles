e6cb3ec55b27090344abfcde7019575f1c7ab284
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-02-09 16:12:40 +0100

    Adding gomod and gowork ts parsers

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,62 +1,64 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  dependencies = {
-    {'windwp/nvim-ts-autotag', opts = {}},
-  },
-  -- cmd = {'TSInstall', 'TSBufEnable', 'TSBufDisable', 'TSModuleInfo'},
-  build = ':TSUpdate',
-  lazy = vim.fn.argc(-1) == 0,
-  config = function()
-    local configs = require('nvim-treesitter.configs')
-    local parsers = require('nvim-treesitter.parsers')
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        {"windwp/nvim-ts-autotag", opts = {}},
+    },
+    cmd = {"TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo"},
+    build = ":TSUpdate",
+    lazy = vim.fn.argc(-1) == 0,
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+        local parsers = require("nvim-treesitter.parsers")
 
-    configs.setup({
-      ensure_installed = {
-        'bash',
-        'css',
-        'diff',
-        'dockerfile',
-        'go',
-        'graphql',
-        'html',
-        'javascript',
-        'jsdoc',
-        'json',
-        'json5',
-        'lua',
-        'markdown',
-        'markdown_inline',
-        'php',
-        'prisma',
-        'python',
-        'regex',
-        'rust',
-        'scss',
-        'sql',
-        'tsx',
-        'typescript',
-        'vim',
-        'vimdoc',
-        'yaml',
-      },
-      highlight = {enable = true},
-      indent = {enable = true},
-      context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<leader>ti',
-          scope_incremental = '<leader>ts',
-          node_incremental = '<leader>ti',
-          node_decremental = '<leader>td',
-        },
-      },
-    })
+        configs.setup({
+            ensure_installed = {
+                "bash",
+                "css",
+                "diff",
+                "dockerfile",
+                "go",
+                "gomod",
+                "gowork",
+                "graphql",
+                "html",
+                "javascript",
+                "jsdoc",
+                "json",
+                "json5",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "php",
+                "prisma",
+                "python",
+                "regex",
+                "rust",
+                "scss",
+                "sql",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            highlight = {enable = true},
+            indent = {enable = true},
+            context_commentstring = {
+                enable = true,
+                enable_autocmd = false,
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<leader>ti",
+                    scope_incremental = "<leader>ts",
+                    node_incremental = "<leader>ti",
+                    node_decremental = "<leader>td",
+                },
+            },
+        })
 
-    local parser_configs = parsers.get_parser_configs();
-    parser_configs.tsx.filetype_to_parsername = {'javascript', 'typescript.tsx'}
-  end,
+        local parser_configs = parsers.get_parser_configs();
+        parser_configs.tsx.filetype_to_parsername = {"javascript", "typescript.tsx"}
+    end,
 }
