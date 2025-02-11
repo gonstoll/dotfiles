044f5ddd29ff7ba4d8cd4e40a4642ca26ccf86ee
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-02-11 12:48:03 +0100

    Don't complete function calls on gopls

--- .config/nvim/lua/plugins/lsp/servers/gopls.lua
+++ .config/nvim/lua/plugins/lsp/servers/gopls.lua
@@ -23,20 +23,20 @@
 -- })
 
 return {
-  settings = {
-    gopls = {
-      -- analyses = {
-      --   unusedparams = true,
-      -- },
-      -- staticcheck = true, -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md#staticcheck-bool
-      -- semanticTokens = true,
-      usePlaceholders = true,
-      completeFunctionCalls = true,
-      analyses = {
-        fillstruct = false,
-        unusedparams = true,
-      },
-      gofumpt = true,
+    settings = {
+        gopls = {
+            -- analyses = {
+            --   unusedparams = true,
+            -- },
+            -- staticcheck = true, -- https://github.com/golang/tools/blob/master/gopls/doc/settings.md#staticcheck-bool
+            -- semanticTokens = true,
+            usePlaceholders = true,
+            completeFunctionCalls = false,
+            analyses = {
+                fillstruct = false,
+                unusedparams = true,
+            },
+            gofumpt = true,
+        },
     },
-  },
 }
