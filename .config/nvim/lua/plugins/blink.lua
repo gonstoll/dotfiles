7e477fbf664b7a3a2723dd3c11cff0016a172702
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-02-20 08:43:05 +0100

    Update `blink.cmp`

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,40 +1,39 @@
 return {
-    'saghen/blink.cmp',
-    version = '*',
-    event = 'InsertEnter',
+    "saghen/blink.cmp",
+    version = "*",
+    event = "InsertEnter",
     enabled = not vim.g.cmp_enable,
     ---@module 'blink.cmp'
     ---@type blink.cmp.Config
     opts = {
         keymap = {
-            preset = 'enter',
-            ['<C-y>'] = {'show', 'show_documentation', 'hide_documentation'},
-            ['<C-u>'] = {'scroll_documentation_up', 'fallback'},
-            ['<C-d>'] = {'scroll_documentation_down', 'fallback'},
-            cmdline = {
-                preset = 'enter',
-                ['<Tab>'] = {'select_next', 'fallback'},
-                ['<S-Tab>'] = {'select_prev', 'fallback'},
+            preset = "enter",
+            ["<C-y>"] = {"show", "show_documentation", "hide_documentation"},
+            ["<C-u>"] = {"scroll_documentation_up", "fallback"},
+            ["<C-d>"] = {"scroll_documentation_down", "fallback"},
+        },
+        cmdline = {
+            keymap = {
+                preset = "enter",
+                ["<Tab>"] = {"select_next", "fallback"},
+                ["<S-Tab>"] = {"select_prev", "fallback"},
             },
         },
         sources = {
-            default = {'lsp', 'path', 'snippets', 'buffer', 'lazydev'},
+            default = {"lsp", "path", "snippets", "buffer", "lazydev"},
             providers = {
-                lsp = {
-                    fallbacks = {'buffer', 'path'},
-                },
                 lazydev = {
-                    name = 'LazyDev',
-                    module = 'lazydev.integrations.blink',
+                    name = "LazyDev",
+                    module = "lazydev.integrations.blink",
                     score_offset = 100, -- show at a higher priority than lsp
                 },
                 snippets = {
-                    name = 'Snippets',
-                    module = 'blink.cmp.sources.snippets',
+                    name = "Snippets",
+                    module = "blink.cmp.sources.snippets",
                     min_keyword_length = 3,
                     opts = {
                         friendly_snippets = false,
-                        search_paths = {vim.fn.stdpath('config') .. '/snippets/nvim'},
+                        search_paths = {vim.fn.stdpath("config") .. "/snippets/nvim"},
                     },
                 },
             },
@@ -42,7 +41,7 @@ return {
         signature = {
             enabled = true,
             window = {
-                border = 'single',
+                border = "single",
                 max_width = math.floor(vim.o.columns * 0.4),
                 max_height = math.floor(vim.o.lines * 0.5),
             },
@@ -60,14 +59,14 @@ return {
                 -- Manual selection, if not in cmdline
                 selection = {
                     preselect = false,
-                    auto_insert = function(ctx) return ctx.mode == 'cmdline' end,
+                    auto_insert = function(ctx) return ctx.mode == "cmdline" end,
                 },
             },
             menu = {
                 draw = {
-                    treesitter = {'lsp'},
+                    treesitter = {"lsp"},
                     columns = {
-                        {'label', gap = 2}, {'kind_icon', gap = 1, 'kind'},
+                        {"label", gap = 2}, {"kind_icon", gap = 1, "kind"},
                     },
                 },
             },
@@ -75,12 +74,12 @@ return {
                 auto_show = true,
                 auto_show_delay_ms = 200,
                 window = {
-                    border = 'none',
+                    border = "none",
                     max_width = math.floor(vim.o.columns * 0.4),
                     max_height = math.floor(vim.o.lines * 0.5),
                 },
             },
         },
     },
-    opts_extend = {'sources.default'},
+    opts_extend = {"sources.default"},
 }
