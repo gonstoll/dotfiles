069212ab705de4a7e379f4ee40314191aae1ba7c
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-03-16 23:24:00 +0100

    Set scroll to 10

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -8,6 +8,7 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 8
+vim.opt.scroll = 10
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
