b3bf37473b2ad956727007669cd3d190279f2136
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-05 12:33:55 +0200

    Deleting comment

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -55,43 +55,6 @@ return {
             end,
         }
 
-        -- Customize prettier args
-        -- require("conform.formatters.prettier").args = function(_, ctx)
-        --     local prettier_roots = {".prettierrc", ".prettierrc.json", "prettier.config.js"}
-        --     local args = {"--stdin-filepath", "$FILENAME"}
-        --     local config_path = vim.fn.stdpath("config")
-        --
-        --     local localPrettierConfig = vim.fs.find(prettier_roots, {
-        --         upward = true,
-        --         path = ctx.dirname,
-        --         type = "file",
-        --     })[1]
-        --     local globalPrettierConfig = vim.fs.find(prettier_roots, {
-        --         path = type(config_path) == "string" and config_path or config_path[1],
-        --         type = "file",
-        --     })[1]
-        --     local disableGlobalPrettierConfig = os.getenv("DISABLE_GLOBAL_PRETTIER_CONFIG")
-        --
-        --     -- Project config takes precedence over global config
-        --     if localPrettierConfig then
-        --         vim.list_extend(args, {"--config", localPrettierConfig})
-        --     elseif globalPrettierConfig and not disableGlobalPrettierConfig then
-        --         -- vim.list_extend(args, {"--config", globalPrettierConfig})
-        --     end
-        --
-        --     local hasTailwindPrettierPlugin = vim.fs.find("node_modules/prettier-plugin-tailwindcss", {
-        --         upward = true,
-        --         path = ctx.dirname,
-        --         type = "directory",
-        --     })[1]
-        --
-        --     if hasTailwindPrettierPlugin then
-        --         vim.list_extend(args, {"--plugin", "prettier-plugin-tailwindcss"})
-        --     end
-        --
-        --     return args
-        -- end
-
         conform.formatters.beautysh = {
             prepend_args = function()
                 return {"--indent-size", "4", "--force-function-style", "fnpar"}
