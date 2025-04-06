f63692d49abf3e1e6946d22147ac86bc0db21a77
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-06 15:27:24 +0200

    Formatting

--- .config/nvim/lua/plugins/colorscheme/kanagawa.lua
+++ .config/nvim/lua/plugins/colorscheme/kanagawa.lua
@@ -1,92 +1,93 @@
 return {
-  'rebelot/kanagawa.nvim',
-  lazy = true,
-  opts = {
-    commentStyle = {italic = false, bold = false},
-    keywordStyle = {italic = false, bold = false},
-    statementStyle = {italic = false, bold = false},
-    functionStyle = {italic = false, bold = false},
-    typeStyle = {italic = false, bold = false},
-    background = {dark = 'dragon', light = 'lotus'},
-    colors = {
-      theme = {
-        all = {
-          ui = {bg_gutter = 'none'},
-          diff = {
-            add = 'none',
-            change = 'none',
-            delete = 'none',
-            text = 'none',
-          },
+    "rebelot/kanagawa.nvim",
+    lazy = true,
+    opts = {
+        commentStyle = {italic = false, bold = false},
+        keywordStyle = {italic = false, bold = false},
+        statementStyle = {italic = false, bold = false},
+        functionStyle = {italic = false, bold = false},
+        typeStyle = {italic = false, bold = false},
+        background = {dark = "dragon", light = "lotus"},
+        colors = {
+            theme = {
+                all = {
+                    ui = {bg_gutter = "none"},
+                    diff = {
+                        add = "none",
+                        change = "none",
+                        delete = "none",
+                        text = "none",
+                    },
+                },
+            },
         },
-      },
+        overrides = function(colors)
+            local theme = colors.theme
+            local palette = colors.palette
+
+            return {
+                Normal = {bg = "#000000"},
+                -- Statusline
+                StatusLine = {bg = theme.ui.bg_p1, fg = theme.syn.fun},
+                --- modes
+                StatusLineAccent = {bg = "none", fg = palette.sakuraPink},
+                StatusLineInsertAccent = {bg = "none", fg = palette.springGreen},
+                StatusLineVisualAccent = {bg = "none", fg = palette.peachRed},
+                StatusLineReplaceAccent = {bg = "none", fg = palette.carpYellow},
+                StatusLineCmdLineAccent = {bg = "none", fg = palette.crystalBlue},
+                StatusLineTerminalAccent = {bg = "none", fg = palette.fujiGray},
+                --- gitsigns
+                StatusLineGitSignsAdd = {bg = theme.ui.bg_p1, fg = theme.vcs.added},
+                StatusLineGitSignsChange = {bg = theme.ui.bg_p1, fg = theme.vcs.changed},
+                StatusLineGitSignsDelete = {bg = theme.ui.bg_p1, fg = theme.vcs.removed},
+                --- diagnostics
+                StatusLineDiagnosticSignError = {bg = theme.ui.bg_p1, fg = palette.peachRed},
+                StatusLineDiagnosticSignWarn = {bg = theme.ui.bg_p1, fg = theme.diag.warning},
+                StatusLineDiagnosticSignInfo = {bg = theme.ui.bg_p1, fg = theme.diag.info},
+                StatusLineDiagnosticSignHint = {bg = theme.ui.bg_p1, fg = theme.diag.hint},
+                StatusLineDiagnosticSignOk = {bg = theme.ui.bg_p1, fg = theme.diag.ok},
+
+                --- floats
+                NormalFloat = {bg = theme.ui.bg_p1},
+                FloatBorder = {fg = theme.ui.shade0, bg = theme.ui.bg_p1},
+                FloatTitle = {fg = theme.ui.shade0, bg = theme.ui.bg_p1, bold = false},
+                FloatFooter = {fg = theme.ui.nontext, bg = theme.ui.bg_p1},
+
+                --- daignostics
+                DiagnosticError = {fg = palette.peachRed},
+                DiagnosticSignError = {fg = palette.peachRed},
+                DiagnosticFloatingError = {fg = palette.peachRed},
+                DiagnosticUnderlineError = {sp = palette.peachRed},
+                DiagnosticVirtualTextError = {fg = palette.peachRed, bg = Utils.colors.blend(palette.peachRed, theme.ui.bg, 0.10)},
+                DiagnosticVirtualTextHint = {fg = theme.diag.hint, bg = Utils.colors.blend(theme.diag.hint, theme.ui.bg, 0.10)},
+                DiagnosticVirtualTextInfo = {fg = theme.diag.info, bg = Utils.colors.blend(theme.diag.info, theme.ui.bg, 0.10)},
+                DiagnosticVirtualTextOk = {fg = theme.diag.ok, bg = Utils.colors.blend(theme.diag.ok, theme.ui.bg, 0.10)},
+                DiagnosticVirtualTextWarn = {fg = theme.diag.warning, bg = Utils.colors.blend(theme.diag.warning, theme.ui.bg, 0.10)},
+
+                --- popups
+                Pmenu = {fg = theme.ui.shade0, bg = theme.ui.bg_p1}, -- add `blend = vim.o.pumblend` to enable transparency
+                PmenuSel = {fg = "NONE", bg = theme.ui.bg_p2},
+                PmenuSbar = {bg = theme.ui.bg_m1},
+                PmenuThumb = {bg = theme.ui.bg_p2},
+
+                --- misc
+                ModeMsg = {fg = theme.syn.comment, bold = false},
+                WinSeparator = {fg = theme.ui.bg_p2},
+                TelescopeSelection = {bg = theme.ui.bg_p2},
+                Cursor = {bg = "none"},
+                CursorLineNr = {bold = false},
+                Title = {bold = false},
+                Boolean = {bold = false},
+                MatchParen = {bold = false, bg = theme.ui.bg_p2},
+                IblIndent = {fg = theme.ui.bg_p2},
+                IblScope = {fg = theme.ui.whitespace},
+                ["@variable.builtin"] = {link = "Special"},
+                ["@lsp.typemod.function.readonly"] = {link = "Function"},
+                ["@boolean"] = {link = "Boolean"},
+                ["@keyword.operator"] = {link = "Keyword"},
+                ["@string.escape"] = {link = "PrePoc"},
+                typescriptParens = {bg = "none"},
+            }
+        end,
     },
-    overrides = function(colors)
-      local theme = colors.theme
-      local palette = colors.palette
-
-      return {
-        -- Statusline
-        StatusLine = {bg = theme.ui.bg_p1, fg = theme.syn.fun},
-        --- modes
-        StatusLineAccent = {bg = 'none', fg = palette.sakuraPink},
-        StatusLineInsertAccent = {bg = 'none', fg = palette.springGreen},
-        StatusLineVisualAccent = {bg = 'none', fg = palette.peachRed},
-        StatusLineReplaceAccent = {bg = 'none', fg = palette.carpYellow},
-        StatusLineCmdLineAccent = {bg = 'none', fg = palette.crystalBlue},
-        StatusLineTerminalAccent = {bg = 'none', fg = palette.fujiGray},
-        --- gitsigns
-        StatusLineGitSignsAdd = {bg = theme.ui.bg_p1, fg = theme.vcs.added},
-        StatusLineGitSignsChange = {bg = theme.ui.bg_p1, fg = theme.vcs.changed},
-        StatusLineGitSignsDelete = {bg = theme.ui.bg_p1, fg = theme.vcs.removed},
-        --- diagnostics
-        StatusLineDiagnosticSignError = {bg = theme.ui.bg_p1, fg = palette.peachRed},
-        StatusLineDiagnosticSignWarn = {bg = theme.ui.bg_p1, fg = theme.diag.warning},
-        StatusLineDiagnosticSignInfo = {bg = theme.ui.bg_p1, fg = theme.diag.info},
-        StatusLineDiagnosticSignHint = {bg = theme.ui.bg_p1, fg = theme.diag.hint},
-        StatusLineDiagnosticSignOk = {bg = theme.ui.bg_p1, fg = theme.diag.ok},
-
-        --- floats
-        NormalFloat = {bg = theme.ui.bg_p1},
-        FloatBorder = {fg = theme.ui.shade0, bg = theme.ui.bg_p1},
-        FloatTitle = {fg = theme.ui.shade0, bg = theme.ui.bg_p1, bold = false},
-        FloatFooter = {fg = theme.ui.nontext, bg = theme.ui.bg_p1},
-
-        --- daignostics
-        DiagnosticError = {fg = palette.peachRed},
-        DiagnosticSignError = {fg = palette.peachRed},
-        DiagnosticFloatingError = {fg = palette.peachRed},
-        DiagnosticUnderlineError = {sp = palette.peachRed},
-        DiagnosticVirtualTextError = {fg = palette.peachRed, bg = Utils.colors.blend(palette.peachRed, theme.ui.bg, 0.10)},
-        DiagnosticVirtualTextHint = {fg = theme.diag.hint, bg = Utils.colors.blend(theme.diag.hint, theme.ui.bg, 0.10)},
-        DiagnosticVirtualTextInfo = {fg = theme.diag.info, bg = Utils.colors.blend(theme.diag.info, theme.ui.bg, 0.10)},
-        DiagnosticVirtualTextOk = {fg = theme.diag.ok, bg = Utils.colors.blend(theme.diag.ok, theme.ui.bg, 0.10)},
-        DiagnosticVirtualTextWarn = {fg = theme.diag.warning, bg = Utils.colors.blend(theme.diag.warning, theme.ui.bg, 0.10)},
-
-        --- popups
-        Pmenu = {fg = theme.ui.shade0, bg = theme.ui.bg_p1}, -- add `blend = vim.o.pumblend` to enable transparency
-        PmenuSel = {fg = 'NONE', bg = theme.ui.bg_p2},
-        PmenuSbar = {bg = theme.ui.bg_m1},
-        PmenuThumb = {bg = theme.ui.bg_p2},
-
-        --- misc
-        ModeMsg = {fg = theme.syn.comment, bold = false},
-        WinSeparator = {fg = theme.ui.bg_p2},
-        TelescopeSelection = {bg = theme.ui.bg_p2},
-        Cursor = {bg = 'none'},
-        CursorLineNr = {bold = false},
-        Title = {bold = false},
-        Boolean = {bold = false},
-        MatchParen = {bold = false, bg = theme.ui.bg_p2},
-        IblIndent = {fg = theme.ui.bg_p2},
-        IblScope = {fg = theme.ui.whitespace},
-        ['@variable.builtin'] = {link = 'Special'},
-        ['@lsp.typemod.function.readonly'] = {link = 'Function'},
-        ['@boolean'] = {link = 'Boolean'},
-        ['@keyword.operator'] = {link = 'Keyword'},
-        ['@string.escape'] = {link = 'PrePoc'},
-        typescriptParens = {bg = 'none'},
-      }
-    end,
-  },
 }
--- .config/nvim/lua/plugins/colorscheme/zenbones.lua
+++ .config/nvim/lua/plugins/colorscheme/zenbones.lua
@@ -28,53 +28,53 @@
 ---@field light LightColorSet
 
 return {
-    'mcchrish/zenbones.nvim',
-    dependencies = {'rktjmp/lush.nvim'},
+    "mcchrish/zenbones.nvim",
+    dependencies = {"rktjmp/lush.nvim"},
     lazy = true,
     config = function()
         vim.g.zenbones_italic_comments = false
         vim.g.rosebones_italic_comments = false
 
-        vim.api.nvim_create_autocmd('ColorScheme', {
-            group = vim.api.nvim_create_augroup('Customize Zenbones', {}),
+        vim.api.nvim_create_autocmd("ColorScheme", {
+            group = vim.api.nvim_create_augroup("Customize Zenbones", {}),
             callback = function()
                 local active_colorscheme = vim.g.colors_name
 
-                if (not string.find(active_colorscheme, 'bones')) then
+                if (not string.find(active_colorscheme, "bones")) then
                     return
                 end
 
                 local theme = require(active_colorscheme)
                 ---@type Palette
-                local palette = require(active_colorscheme .. '.palette')
-                local lush = require('lush')
+                local palette = require(active_colorscheme .. ".palette")
+                local lush = require("lush")
                 local bg = vim.o.background
-                local statusline_bg = bg == 'dark' and palette.dark.bg_warm.li(10) or palette.light.bg1
+                local statusline_bg = bg == "dark" and palette.dark.bg_warm.li(10) or palette.light.bg1
 
                 ---@diagnostic disable: undefined-global
                 local specs = lush.parse(function()
                     return {
                         -- Normal({theme.Normal, bg = bg == 'dark' and '#181616' or palette[bg].bg}),
-                        Normal({theme.Normal, bg = bg == 'dark' and '#000000' or palette[bg].bg}),
+                        Normal({theme.Normal, bg = bg == "dark" and "#000000" or palette[bg].bg}),
                         -- NormalNC({theme.NormalNC, bg = bg == 'dark' and '#2d2929' or palette[bg].bg}),
                         -- NormalNC({theme.NormalNC, bg = bg == 'dark' and '#1f1d1d' or palette[bg].bg.da(10)}),
-                        NormalNC({theme.NormalNC, bg = bg == 'dark' and '#262323' or palette[bg].bg.da(10)}),
-                        TermCursor({cterm = 'reverse', gui = 'reverse'}),
-                        Cursor({theme.Cursor, bg = bg == 'dark' and '#ffffff' or '#000000'}),
+                        NormalNC({theme.NormalNC, bg = bg == "dark" and "#262323" or palette[bg].bg.da(10)}),
+                        TermCursor({cterm = "reverse", gui = "reverse"}),
+                        Cursor({theme.Cursor, bg = bg == "dark" and "#ffffff" or "#000000"}),
                         FloatBorder({theme.FloatBorder, bg = theme.NormalFloat.bg}),
                         FloatTitle({theme.FloatTitle, bg = theme.NormalFloat.bg}),
-                        Special({theme.Special, gui = 'normal'}),
-                        Statement({theme.Statement, gui = 'normal'}),
-                        Directory({theme.Directory, gui = 'normal'}),
+                        Special({theme.Special, gui = "normal"}),
+                        Statement({theme.Statement, gui = "normal"}),
+                        Directory({theme.Directory, gui = "normal"}),
                         Boolean({
                             theme.Boolean,
-                            gui = 'bold',
-                            fg = active_colorscheme == 'rosebones' and palette[bg].rose or theme.Boolean.fg,
+                            gui = "bold",
+                            fg = active_colorscheme == "rosebones" and palette[bg].rose or theme.Boolean.fg,
                         }),
                         SnippetTabStop({theme.SnippetTabStop, bg = theme.Normal.bg}),
                         Function({
                             theme.Function,
-                            fg = bg == 'dark' and palette.dark.bg.li(58) or
+                            fg = bg == "dark" and palette.dark.bg.li(58) or
                                 palette.light.bg.sa(20).da(60),
                         }),
                         TodoBgTODO({theme.TodoBgTODO, bg = palette[bg].water, fg = palette[bg].fg}),
@@ -96,9 +96,9 @@ return {
 
                 lush.apply(lush.compile(specs))
 
-                if (active_colorscheme == 'rosebones') then
-                    vim.api.nvim_set_hl(0, '@tag.tsx', {link = 'Function'})
-                    vim.api.nvim_set_hl(0, '@tag.builtin.tsx', {link = 'Function'})
+                if (active_colorscheme == "rosebones") then
+                    vim.api.nvim_set_hl(0, "@tag.tsx", {link = "Function"})
+                    vim.api.nvim_set_hl(0, "@tag.builtin.tsx", {link = "Function"})
                 end
             end,
         })
