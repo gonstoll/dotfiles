7959c8e338cf737515302ebc526c8300b85644b8
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-06 13:36:20 +0200

    Trying out avante

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -44,4 +44,37 @@ return {
             debug = false,
         },
     },
+
+    {
+        "yetone/avante.nvim",
+        version = false,
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "stevearc/dressing.nvim",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "ibhagwan/fzf-lua",
+            "nvim-tree/nvim-web-devicons",
+            "zbirenbaum/copilot.lua",
+        },
+        keys = {
+            {"<leader>ak", ":AvanteChat<CR>", desc = "Avante: Chat"},
+        },
+        opts = {
+            provider = "gemini",
+            windows = {
+                postion = "right",
+                width = 40,
+                sidebar_header = {
+                    enabled = true,
+                    align = "left",
+                    rounded = false,
+                },
+            },
+            file_selector = {
+                provider = "fzf",
+            },
+        },
+        build = "make",
+    },
 }
