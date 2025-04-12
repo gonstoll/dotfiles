ec753404c11d3662783c680e6fbe54a2dc1bf1c7
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-12 22:10:50 +0200

    Blink strikes back

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,7 +2,7 @@
 -- https://github.com/neovim/neovim/issues/17070#issuecomment-1086775760
 -- vim.loop.fs_write(2, '\27Ptmux;\27\27]11;?\7\27\\', -1, nil)
 
-vim.g.blink_enabled = false
+vim.g.blink_enabled = true
 _G.Utils = require("utils")
 
 -- https://github.com/neovim/neovim/issues/31675
--- .config/nvim/lua/plugins/cmp/blink.lua
+++ .config/nvim/lua/plugins/cmp/blink.lua
@@ -1,6 +1,6 @@
 return {
     "saghen/blink.cmp",
-    version = "*",
+    version = "1.*",
     event = "InsertEnter",
     enabled = vim.g.blink_enabled,
     ---@module 'blink.cmp'
