367642e983ef98b2fa4bd3e63cc4c0c8c432b3e6
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-04-13 19:38:03 +0200

    Adding `gofmt` to go formatter list

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,31 +1,32 @@
 return {
     "stevearc/conform.nvim",
-    event = {"BufWritePre"},
-    cmd = {"ConformInfo"},
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
     opts = {
         quiet = true,
         formatters_by_ft = {
-            typescript = {"prettier"},
-            typescriptreact = {"prettier"},
-            javascript = {"prettier"},
-            javascriptreact = {"prettier"},
-            html = {"prettier"},
-            css = {"prettier"},
-            scss = {"prettier"},
-            lua = {'stylua'},
-            markdown = {"prettier"},
-            yaml = {"prettier"},
-            graphql = {"prettier"},
-            vue = {"prettier"},
-            angular = {"prettier"},
-            less = {"prettier"},
-            flow = {"prettier"},
-            sh = {"beautysh"},
-            bash = {"beautysh"},
-            zsh = {"beautysh"},
-            http = {"kulala-fmt"},
-            python = {"black"},
-            ["_"] = {"trim_whitespace"},
+            typescript = { "prettier" },
+            typescriptreact = { "prettier" },
+            javascript = { "prettier" },
+            javascriptreact = { "prettier" },
+            html = { "prettier" },
+            css = { "prettier" },
+            scss = { "prettier" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            yaml = { "prettier" },
+            graphql = { "prettier" },
+            vue = { "prettier" },
+            angular = { "prettier" },
+            less = { "prettier" },
+            flow = { "prettier" },
+            sh = { "beautysh" },
+            bash = { "beautysh" },
+            zsh = { "beautysh" },
+            http = { "kulala-fmt" },
+            python = { "black" },
+            go = { "gofmt" },
+            ["_"] = { "trim_whitespace" },
         },
         format_on_save = function(bufnr)
             -- Disable autoformat for files in a certain path
@@ -34,9 +35,9 @@ return {
                 return
             end
 
-            return {timeout_ms = 1000, lsp_fallback = true}
+            return { timeout_ms = 1000, lsp_fallback = true }
         end,
-        format_after_save = {lsp_fallback = true},
+        format_after_save = { lsp_fallback = true },
         log_level = vim.log.levels.DEBUG,
         formatters = {
             prettier = {
@@ -47,7 +48,6 @@ return {
                         "--no-bracket-spacing",
                         "--print-width",
                         "80",
-                        "--with-node-modules",
                         "--config-precedence",
                         "prefer-file",
                     }
@@ -55,7 +55,7 @@ return {
             },
             beautysh = {
                 prepend_args = function()
-                    return {"--indent-size", "4", "--force-function-style", "fnpar"}
+                    return { "--indent-size", "4", "--force-function-style", "fnpar" }
                 end,
             },
         },
