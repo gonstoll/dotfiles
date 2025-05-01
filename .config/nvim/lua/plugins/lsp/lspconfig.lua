e6656138d038280cfd44c0392e019d40f5ba2392
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-05-01 19:22:47 +0200

    Tailwind config not needed anymore

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -5,10 +5,10 @@ return {
         local setup_server = require("lsp").setup_server
         setup_server("stylelint_lsp")
         setup_server("basedpyright")
+        setup_server("tailwindcss")
         setup_server("jsonls", require("plugins.lsp.servers.jsonls"))
         setup_server("yamlls", require("plugins.lsp.servers.yamlls"))
         setup_server("bashls", require("plugins.lsp.servers.bashls"))
-        setup_server("tailwindcss", require("plugins.lsp.servers.tailwindcss"))
         setup_server("cssls", require("plugins.lsp.servers.cssls"))
         setup_server("eslint", require("plugins.lsp.servers.eslint"))
         setup_server("lua_ls", require("plugins.lsp.servers.lua_ls"))
--- .config/nvim/lua/plugins/lsp/servers/tailwindcss.lua
+++
@@ -1,6 +0,0 @@
-return {
-    root_dir = function(fname)
-        local install_path = "node_modules/tailwindcss"
-        return vim.fs.find(install_path, { upward = true, path = fname, type = "directory" })[1]
-    end,
-}
