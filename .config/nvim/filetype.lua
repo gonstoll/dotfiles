4ced11d875e98498c366144536f9e275538e3794
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-05-31 17:16:47 +0200

    Updating plugins

---
+++ .config/nvim/filetype.lua
@@ -0,0 +1,18 @@
+vim.filetype.add({
+    filename = {
+        [".eslintrc.json"] = "jsonc",
+    },
+    pattern = {
+        ["tsconfig*.json"] = "jsonc",
+        [".*/%.vscode/.*%.json"] = "jsonc",
+        -- Borrowed from LazyVim. Mark huge files to disable features later.
+        [".*"] = function(path, bufnr)
+            return vim.bo[bufnr]
+                    and vim.bo[bufnr].filetype ~= "bigfile"
+                    and path
+                    and vim.fn.getfsize(path) > (1024 * 500) -- 500 KB
+                    and "bigfile"
+                or nil
+        end,
+    },
+})
