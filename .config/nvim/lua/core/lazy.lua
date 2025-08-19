1b77ad4ec3d9100f673c8820b11ee61c1c16c6b3
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-08-19 18:40:07 +0200

    Blink wins

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -1,12 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    local out = vim.fn.system({"git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath})
     if vim.v.shell_error ~= 0 then
         vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
+            {"Failed to clone lazy.nvim:\n", "ErrorMsg"},
+            {out, "WarningMsg"},
+            {"\nPress any key to exit..."},
         }, true, {})
         vim.fn.getchar()
         os.exit(1)
@@ -16,19 +16,18 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
-        { import = "plugins" },
-        { import = "plugins.cmp" },
-        { import = "plugins.colorscheme" },
+        {import = "plugins"},
+        {import = "plugins.colorscheme"},
     },
     change_detection = {
         notify = false,
         enabled = true,
     },
     install = {
-        colorscheme = { "rose-pine" },
+        colorscheme = {"rose-pine"},
     },
     performance = {
-        cache = { enabled = true },
+        cache = {enabled = true},
         rtp = {
             disabled_plugins = {
                 "gzip",
