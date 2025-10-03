aad6f350ecfee89a7676ac2e81985a2b12977700
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-10-03 19:14:46 +0200

    Harpoon changes

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,82 +1,10 @@
--- A lot here was taken from https://github.com/mike-jl/harpoonEx
 local desc = Utils.plugin_keymap_desc("harpoon")
-
-local globalIndex = {}
-
-local function get_full_path(path)
-    local bufPath = vim.uv.fs_realpath(path)
-    if not bufPath then
-        if string.sub(path, 1, 1) == "/" then
-            bufPath = path
-        else
-            bufPath = vim.uv.fs_realpath(".") .. "/" .. path
-        end
-    end
-    return bufPath
-end
-
-local function sync_index(list, index)
-    if index then
-        globalIndex[list.name] = index
-        return
-    end
-
-    if globalIndex[list.name] > list._length then
-        globalIndex[list.name] = list._length
-        return
-    end
-    local bufPath = get_full_path(vim.api.nvim_buf_get_name(0))
-    -- find corresponding harpoon item
-    for i = 1, list._length do
-        local item = list.items[i]
-        if item ~= nil and item.value ~= nil then
-            local harpoonPath = get_full_path(item.value)
-            if bufPath == harpoonPath then
-                globalIndex[list.name] = i
-                return
-            end
-        end
-    end
-end
-
-local function cur_buf_is_harpoon(list)
-    local current = list.items[globalIndex[list.name]]
-    if not current then
-        return true
-    end
-
-    local bufnr = vim.fn.bufnr(get_full_path(current.value))
-
-    return bufnr == vim.fn.bufnr()
-end
-
-local function next_harpoon(list, prev)
-    if not cur_buf_is_harpoon(list) then
-        list:select(globalIndex[list.name])
-        return
-    end
-    local i = globalIndex[list.name] or 0
-    local j = 1
-    while true do
-        if prev then
-            i = i - 1
-        else
-            i = i + 1
-        end
-        if list.items[i] then
-            list:select(i)
-            return
-        end
-        j = j + 1
-        if j > list._length then
-            return
-        end
-    end
-end
 
 local keys = {
     open_menu = "<leader>hh",
     add_to_list = "<leader>ha",
+    prev_file = "<S-h>",
+    next_file = "<S-l>",
     sel_f1 = "<leader>h1",
     sel_f2 = "<leader>h2",
     sel_f3 = "<leader>h3",
@@ -86,8 +14,6 @@ local keys = {
     sel_f7 = "<leader>h7",
     sel_f8 = "<leader>h8",
     sel_f9 = "<leader>h9",
-    prev_file = "<S-h>",
-    next_file = "<S-l>",
 }
 
 return {
@@ -111,22 +37,11 @@ return {
     },
     config = function()
         local harpoon = require("harpoon")
-        -- local extensions = require('harpoon.extensions')
-        ---@type HarpoonToggleOptions
-        local menu_opts = {border = "single"}
-
+        local harpoon_extensions = require("harpoon.extensions")
         harpoon:setup({settings = {save_on_toggle = true, sync_on_ui_close = true}})
+        harpoon:extend(harpoon_extensions.builtins.highlight_current_file())
         -- harpoon:extend(extensions.builtins.command_on_nav('UfoEnableFold'))
         harpoon:extend({
-            SELECT = function(args)
-                sync_index(args.list, args.idx)
-            end,
-            ADD = function(args)
-                sync_index(args.list, args.idx)
-            end,
-            REMOVE = function(args)
-                sync_index(args.list)
-            end,
             UI_CREATE = function(cx)
                 vim.keymap.set(
                     "n",
@@ -146,32 +61,17 @@ return {
                     function() harpoon.ui:select_menu_item({tabedit = true}) end,
                     {desc = desc("Open in new tab"), buffer = cx.bufnr}
                 )
-
-                for line_number, file in pairs(cx.contents) do
-                    if string.find(cx.current_file, file, 1, true) then
-                        -- highlight the harpoon menu line that corresponds to the current buffer
-                        vim.api.nvim_buf_add_highlight(
-                            cx.bufnr,
-                            -1,
-                            "CursorLineNr",
-                            line_number - 1,
-                            0,
-                            -1
-                        )
-                        -- set the position of the cursor in the harpoon menu to the start of the current buffer line
-                        vim.api.nvim_win_set_cursor(cx.win_id, {line_number, 0})
-                    end
-                end
             end,
         })
+
+        ---@type HarpoonToggleOptions
+        local menu_opts = {border = "single"}
 
         vim.keymap.set("n", keys.open_menu, function() harpoon.ui:toggle_quick_menu(harpoon:list(), menu_opts) end,
             {desc = desc("Open menu")})
         vim.keymap.set("n", keys.add_to_list, function() harpoon:list():add() end, {desc = desc("Add to list")})
-        vim.keymap.set("n", keys.prev_file, function() next_harpoon(harpoon:list(), true) end,
-            {desc = desc("Previous file")})
-        vim.keymap.set("n", keys.next_file, function() next_harpoon(harpoon:list(), false) end,
-            {desc = desc("Next file")})
+        vim.keymap.set("n", keys.prev_file, function() harpoon:list():prev() end, {desc = desc("Previous file")})
+        vim.keymap.set("n", keys.next_file, function() harpoon:list():next() end, {desc = desc("Next file")})
         vim.keymap.set("n", keys.sel_f1, function() harpoon:list():select(1) end, {desc = desc("Select file 1")})
         vim.keymap.set("n", keys.sel_f2, function() harpoon:list():select(2) end, {desc = desc("Select file 2")})
         vim.keymap.set("n", keys.sel_f3, function() harpoon:list():select(3) end, {desc = desc("Select file 3")})
