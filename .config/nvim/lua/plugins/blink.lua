14578699c6a0f9c3c9f845e3b1989e323fce779a
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-10-28 21:28:12 +0100

    Blink config correction

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -15,9 +15,6 @@ return {
                 function() -- sidekick next edit suggestion
                     return require("sidekick").nes_jump_or_apply()
                 end,
-                function() -- if you are using Neovim's native inline completions
-                    return vim.lsp.inline_completion.get()
-                end,
                 "fallback",
             },
         },
