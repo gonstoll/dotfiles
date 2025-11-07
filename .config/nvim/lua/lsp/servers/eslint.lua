bea73c2456079c22f7ef2fb5818a6122df110e8b
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-11-07 19:26:35 +0100

    Update eslint config

--- .config/nvim/lua/lsp/servers/eslint.lua
+++ .config/nvim/lua/lsp/servers/eslint.lua
@@ -3,10 +3,9 @@ local desc = Utils.plugin_keymap_desc("eslint")
 return {
     settings = {
         format = false,
-        workingDirectories = {mode = "auto"}, -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
     },
     flags = os.getenv("DEBOUNCE_ESLINT") and {
-        -- allow_incremental_sync = false,
+        allow_incremental_sync = false,
         debounce_text_changes = 1000,
     } or nil,
     keys = {
