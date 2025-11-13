3c039ebd5d6bfe1ee58f96d7b202f24f681ceec5
Gonzalo Stoll <stollgonzalo@gmail.com>
2025-11-13 01:07:16 +0100

    Local obsidian vault keymaps

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -72,3 +72,30 @@ autocmd("BufWritePost", {
         end
     end,
 })
+
+autocmd({"BufRead", "BufNewFile"}, {
+    pattern = vim.fn.expand("~") .. "/notes/obsidian/gonzalo_notes/new/*.md",
+    group = augroup("GonzaloNotesNew", {}),
+    callback = function()
+        vim.keymap.set("n", "<leader>ok", function()
+            local flushed_dir = "_flushed"
+            local target_dir = vim.fn.expand("~") .. "/notes/obsidian/gonzalo_notes/" .. flushed_dir
+            if vim.fn.isdirectory(target_dir) == 0 then
+                vim.cmd("silent! !mkdir -p " .. target_dir)
+            end
+            vim.cmd("silent! !mv '%:p' " .. target_dir .. "/'%:t'")
+            vim.cmd("bd")
+        end, {desc = "Obsidian: Accept note and move it to flushed directory"})
+
+        vim.keymap.set("n", "<leader>od", ":silent! !rm '%:p'<cr>:bd<cr>", {desc = "Obsidian: Discard note"})
+
+        vim.keymap.set("n", "<leader>of", function()
+            -- Search for the first heading line
+            vim.cmd("/^# 20")
+            -- Remove date prefix from the heading
+            vim.cmd([[s/^# \d\{4}-\d\{2}-\d\{2}_\(.*\)/# \1/]])
+            -- Replace underscores with spaces in the heading
+            vim.cmd([[s/_/ /g]])
+        end, {desc = "Format note title"})
+    end,
+})
