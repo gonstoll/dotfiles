8d06995e2e54073d792f05f19e167d70e3a902d2
Gonzalo Stoll <25900010+gonstoll@users.noreply.github.com>
2025-12-12 14:11:10 +0100

    Bring back `octo`

---
+++ .config/nvim/lua/plugins/octo.lua
@@ -0,0 +1,30 @@
+local desc = Utils.plugin_keymap_desc("octo")
+
+return {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    opts = {
+        picker = "fzf-lua",
+        enable_builtin = true,
+        default_to_projects_v2 = true,
+        default_merge_method = "squash",
+    },
+    keys = {
+        {"<leader>q", "", desc = "+octo"},
+        {"<leader>qi", "<CMD>Octo issue list<CR>", desc = desc("List GitHub Issues")},
+        {"<leader>qI", "<cmd>Octo issue search<CR>", desc = desc("Search Issues")},
+        {"<leader>qp", "<CMD>Octo pr list<CR>", desc = desc("List GitHub Pull Requests")},
+        {"<leader>qP", "<CMD>Octo pr search<CR>", desc = desc("Search GitHub Pull Requests")},
+        {"<leader>qd", "<CMD>Octo discussion list<CR>", desc = desc("List GitHub Discussions")},
+        {"<leader>qn", "<CMD>Octo notification list<CR>", desc = desc("List GitHub Notifications")},
+        {"<leader>qr", "<cmd>Octo repo list<CR>", desc = desc("List Repos")},
+        {"<leader>qS", "<cmd>Octo search<CR>", desc = desc("Search")},
+        {
+            "<leader>qs",
+            function()
+                require("octo.utils").create_base_search_command {include_current_repo = true}
+            end,
+            desc = desc("Search GitHub"),
+        },
+    },
+}
