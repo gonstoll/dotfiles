4a7e1b6473654d288fd163f1ed2f6d6c556ce7ae
Gonzalo Stoll <stollgonzalo@gmail.com>
2026-01-03 14:51:06 +0100

    Add catpuccin as a second theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,46 +1,70 @@
 return {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    priority = 1000,
-    opts = {
-        styles = {italic = false},
-        dim_inactive_windows = true,
-        palette = {
-            dawn = {
-                no_bg = "#faf4ed",
-                cursor_bg = "#998f97",
-                cursor_fg = "#575279",
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 1000,
+        opts = {
+            styles = {italic = false},
+            dim_inactive_windows = true,
+            palette = {
+                dawn = {
+                    no_bg = "#faf4ed",
+                    cursor_bg = "#998f97",
+                    cursor_fg = "#575279",
+                },
+                moon = {
+                    -- gold = "#f6d5a7",
+                    -- foam = "#a1d1da",
+                    -- iris = "#d9c7ef",
+                    -- rose = "#ebbcba",
+                    -- pine = "#437e91",
+                    no_bg = "#141415",
+                    cursor_bg = "#ffffff",
+                    cursor_fg = "#000000",
+                },
+                main = {
+                    no_bg = "#141415",
+                    cursor_bg = "#ffffff",
+                    cursor_fg = "#000000",
+                },
             },
-            moon = {
-                -- gold = "#f6d5a7",
-                -- foam = "#a1d1da",
-                -- iris = "#d9c7ef",
-                -- rose = "#ebbcba",
-                -- pine = "#437e91",
-                no_bg = "#141415",
-                cursor_bg = "#ffffff",
-                cursor_fg = "#000000",
+            highlight_groups = {
+                Normal = {bg = "no_bg"},
+                Cursor = {bg = "cursor_bg", fg = "cursor_fg"},
+                Directory = {fg = "foam", bold = false},
+                StatusLine = {bg = "no_bg", fg = "clear"},
+                StatusLineTerm = {bg = "base", fg = "subtle"},
+                StatusLineNC = {bg = "no_bg", fg = "subtle"},
             },
-            main = {
-                no_bg = "#141415",
-                cursor_bg = "#ffffff",
-                cursor_fg = "#000000",
-            },
-        },
-        highlight_groups = {
-            Normal = {bg = "no_bg"},
-            Cursor = {bg = "cursor_bg", fg = "cursor_fg"},
-            Directory = {fg = "foam", bold = false},
-            StatusLine = {bg = "no_bg", fg = "clear"},
-            StatusLineTerm = {bg = "base", fg = "subtle"},
-            StatusLineNC = {bg = "no_bg", fg = "subtle"},
-        },
 
-        before_highlight = function(group, highlight, palette)
-            if group:match("^DiagnosticVirtualText") and highlight.bg == nil then
-                highlight.bg = highlight.fg
-                highlight.blend = 10
-            end
-        end,
+            before_highlight = function(group, highlight, palette)
+                if group:match("^DiagnosticVirtualText") and highlight.bg == nil then
+                    highlight.bg = highlight.fg
+                    highlight.blend = 10
+                end
+            end,
+        },
+    },
+
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = true,
+        opts = {
+            transparent_background = true,
+            term_colors = true,
+            no_italic = true,
+            no_bold = true,
+            lsp_styles = {
+                underlines = {
+                    errors = {"undercurl"},
+                    hints = {"undercurl"},
+                    warnings = {"undercurl"},
+                    information = {"undercurl"},
+                    ok = {"undercurl"},
+                },
+            },
+            auto_integrations = true,
+        },
     },
 }
