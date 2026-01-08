887af6505912e95f671aaf0f8a73b051c5d83e1e
Gonzalo Stoll <25900010+gonstoll@users.noreply.github.com>
2026-01-08 16:45:15 +0100

    Octo config updates

--- .config/nvim/lua/plugins/octo.lua
+++ .config/nvim/lua/plugins/octo.lua
@@ -8,6 +8,9 @@ return {
         enable_builtin = true,
         default_to_projects_v2 = true,
         default_merge_method = "squash",
+        picker_config = {
+            use_emojis = true,
+        },
     },
     keys = {
         {"<leader>q", "", desc = "+octo"},
@@ -15,6 +18,8 @@ return {
         {"<leader>qI", "<cmd>Octo issue search<CR>", desc = desc("Search Issues")},
         {"<leader>qp", "<CMD>Octo pr list<CR>", desc = desc("List GitHub Pull Requests")},
         {"<leader>qP", "<CMD>Octo pr search<CR>", desc = desc("Search GitHub Pull Requests")},
+        {"<leader>qc", "<CMD>tabnew | Octo pr create<CR>", desc = desc("Create GitHub Pull Request")},
+        {"<leader>qb", "<CMD>Octo pr browser<CR>", desc = desc("Open Pull Request on browser")},
         {"<leader>qd", "<CMD>Octo discussion list<CR>", desc = desc("List GitHub Discussions")},
         {"<leader>qn", "<CMD>Octo notification list<CR>", desc = desc("List GitHub Notifications")},
         {"<leader>qr", "<cmd>Octo repo list<CR>", desc = desc("List Repos")},
@@ -22,7 +27,7 @@ return {
         {
             "<leader>qs",
             function()
-                require("octo.utils").create_base_search_command {include_current_repo = true}
+                require("octo.utils").create_base_search_command({include_current_repo = true})
             end,
             desc = desc("Search GitHub"),
         },
