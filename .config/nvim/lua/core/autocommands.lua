3d35bc534019524b9ff4f4ac9e67a229a48f4968
Gonzalo Stoll <stollgonzalo@gmail.com>
2026-04-12 20:35:00 +0200

    Move treesitter autocommand to autocommands file

--- .config/nvim/lua/core/autocommands.lua
+++ .config/nvim/lua/core/autocommands.lua
@@ -111,3 +111,25 @@ autocmd({"BufRead", "BufNewFile"}, {
         end, {desc = "Format note title"})
     end,
 })
+
+autocmd("FileType", {
+    callback = function(args)
+        local buf, filetype = args.buf, args.match
+
+        local language = vim.treesitter.language.get_lang(filetype)
+        if not language then return end
+
+        -- check if parser exists and load it
+        if not vim.treesitter.language.add(language) then return end
+        -- enables syntax highlighting and other treesitter features
+        vim.treesitter.start(buf, language)
+
+        -- enables treesitter based folds
+        -- for more info on folds see `:help folds`
+        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        vim.wo.foldmethod = "expr"
+
+        -- enables treesitter based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     version = false,
     dependencies = {
         {"windwp/nvim-ts-autotag", opts = {}},
-        {"nvim-treesitter/nvim-treesitter-context", opts = {enable = false}},
+        {"nvim-treesitter/nvim-treesitter-context", opts = {enable = false, max_lines = 3}},
     },
     cmd = {"TSUpdate", "TSInstall", "TSLog", "TSUninstall"},
     build = ":TSUpdate",
@@ -18,9 +18,7 @@ return {
     },
     config = function(_, opts)
         local TS = require("nvim-treesitter")
-
         TS.setup(opts)
-
         TS.install({
             "bash",
             "css",
@@ -51,28 +49,6 @@ return {
             "vimdoc",
             "yaml",
             "toml",
-        })
-
-        vim.api.nvim_create_autocmd("FileType", {
-            callback = function(args)
-                local buf, filetype = args.buf, args.match
-
-                local language = vim.treesitter.language.get_lang(filetype)
-                if not language then return end
-
-                -- check if parser exists and load it
-                if not vim.treesitter.language.add(language) then return end
-                -- enables syntax highlighting and other treesitter features
-                vim.treesitter.start(buf, language)
-
-                -- enables treesitter based folds
-                -- for more info on folds see `:help folds`
-                vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                vim.wo.foldmethod = "expr"
-
-                -- enables treesitter based indentation
-                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-            end,
         })
     end,
 }
